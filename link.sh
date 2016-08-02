3ff1cdda30dc13c01e9aeee5e507e3978fd86a22
wacky515 <wacky515@gmail.com>
2016-08-03 07:28:00 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -2,13 +2,24 @@
 
 cd ${DOT_DIRECTORY}
 
-
 for f in .??*
+
+# 無視したいファイルやディレクトリは以下に追記
 do
-    # 無視したいファイルやディレクトリは以下の様に追記する
-    [[ ${f} = ".git" ]] && continue
+    [[ ${f} = "README.md" ]] && continue
+    [[ ${f} = "*. ~" ]] && continue
+    [[ ${f} = "*.un~" ]] && continue
+    [[ ${f} = "*.swp" ]] && continue
+
+    [[ ${f} = "*.old" ]] && continue
+
+    [[ ${f} = "*.sh" ]] && continue
+    [[ ${f} = ".modfying" ]] && continue
     [[ ${f} = ".gitignore" ]] && continue
+
+    [[ ${f} = ".git" ]] && continue
   
     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
+
 echo $(tput setaf 2)Deploy dotfiles complete!. ??$(tput sgr0)
