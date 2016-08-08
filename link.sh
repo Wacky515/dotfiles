290c370905e0263b82be34143b54f4187e4799bb
wacky515 <wacky515@gmail.com>
2016-08-09 07:14:17 +0900

    Update .gitconfig, link.sh

--- link.sh
+++ link.sh
@@ -5,7 +5,7 @@
 
 echo Start link
 
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig | $!
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim ~/.vim
@@ -18,31 +18,31 @@ echo Done link
 # do
 #     [[ "$f" == ".git" ]] && continue
 #     [[ "$f" == ".DS_Store" ]] && continue
-# 
+
 #     echo "$f"
 # done
 
 
 # cd ${DOT_DIRECTORY}
-# 
+
 # for f in .??*
-# 
+
 # # 無視したいファイルやディレクトリは以下に追記
 # do
 #     [[ ${f} = "README.md" ]] && continue
 #     [[ ${f} = "*. ~" ]] && continue
 #     [[ ${f} = "*.un~" ]] && continue
 #     [[ ${f} = "*.swp" ]] && continue
-# 
+
 #     [[ ${f} = "*.old" ]] && continue
-# 
+
 #     [[ ${f} = "*.sh" ]] && continue
 #     [[ ${f} = ".modfying" ]] && continue
 #     [[ ${f} = ".gitignore" ]] && continue
-# 
+
 #     [[ ${f} = ".git" ]] && continue
-#   
+
 #     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 # done
-# 
+
 # echo $(tput setaf 2)Deploy dotfiles complete!. ??$(tput sgr0)
