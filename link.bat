d20aa2ecc2998bd30a161d00c953140c9ba77838
wacky515 <wacky515@gmail.com>
2016-08-13 14:45:48 +0900

    Update link.bat

--- link.bat
+++ link.bat
@@ -1,4 +1,4 @@
-echo Start link
+rem Start link
 
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
 
@@ -6,17 +6,19 @@ mklink %HOMEPATH%"\.vimrc" ".\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" ".\dotfiles\.gvimrc"
 mklink /D %HOMEPATH%"\.vim" ".\dotfiles\.vim"
 
-rem /*{{{*/
-rem mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+@rem /*{{{*/
+@rem mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 
-rem mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-rem mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
-rem mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
+@rem mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+@rem mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
+@rem mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 
-rem mklink /D %HOMEPATH%"\.vim\userautoload" %HOMEPATH%"\dotfiles\.vim\userautoload"
-rem mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\.vim\dotfiles\colors"
-rem /*}}}*/
+@rem mklink /D %HOMEPATH%"\.vim\userautoload" %HOMEPATH%"\dotfiles\.vim\userautoload"
+@rem mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\.vim\dotfiles\colors"
+@rem /*}}}*/
 
-echo End link
+rem End link
+
+pause
 
 exit 0
