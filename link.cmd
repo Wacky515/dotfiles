a8c4653182dd1893aeafb47868ad2e08b43827d7
wacky515 <wacky515@gmail.com>
2016-10-11 07:51:19 +0900

    Update link.cmd

--- link.cmd
+++ link.cmd
@@ -2,7 +2,8 @@ echo off
 echo Start link
 
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
-    if not %ERRORLEVEL% == "0" (
+    rem echo %ERRORLEVEL%
+    if %ERRORLEVEL% == 0 (
         echo ".gitconfig link success!"
         echo This PC name: %COMPUTERNAME%
         for /f "delims=" %%i in (office_pc.txt) do (
@@ -10,19 +11,20 @@ mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
             if /i %%i == %COMPUTERNAME% (
                 goto set_proxy
                 )
-            )
+	    )
         echo "In normal network"
-        goto normal_netwoek
+        goto end_gitconfig
+	) else (
+	rem ) else if %ERRORLEVEL% == "0" (
+    goto end_gitconfig)
 
-        )
-    )
 :set_proxy
 echo "In proxy, set proxy"
-git config --list
-rem git config --system http.proxy http://proxy.intra.xacti-co.com:8080
-rem git config --system https.proxy https://proxy.intra.xacti-co.com:8080
+rem git config --list
+git config --system http.proxy http://proxy.intra.xacti-co.com:8080
+git config --system https.proxy https://proxy.intra.xacti-co.com:8080
 
-:normal_netwoek
+:end_gitconfig
 
 mklink %HOMEPATH%"\.gitignore" ".\dotfiles\.gitignore"
 
