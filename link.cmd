39491da3607b063160c9263b0ebe540201d341fc
wacky515 <wacky515@gmail.com>
2016-10-20 07:57:25 +0900

    Update link.cmd

--- link.cmd
+++ link.cmd
@@ -1,12 +1,13 @@
 @echo off
-echo Start link
 
-rem "link.cmd" ‚ÌƒtƒHƒ‹ƒ_‚ÉˆÚ“®
+echo Start set link
+
+rem "link.cmd" ‚ª‚ ‚éDir‚ð "cd" ‚É‚·‚é
 cd /d %~dp0
 
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
-    rem echo %ERRORLEVEL%
-    if %ERRORLEVEL% == 0 (
+rem echo %ERRORLEVEL%
+if %ERRORLEVEL% == 0 (
         echo ".gitconfig link success!"
         echo This PC name: %COMPUTERNAME%
         for /f "delims=" %%i in (C:.\office_pc.txt) do (
@@ -14,27 +15,27 @@ mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
             if /i %%i == %COMPUTERNAME% (
                 goto set_proxy
                 )
-	    )
+            )
         echo "In normal network"
-        goto end_gitconfig
-	) else (
-    goto end_gitconfig)
+        goto end_gitconfig) else (
+            goto end_gitconfig
+            )
 
-:set_proxy
-echo "In proxy, set proxy"
-rem git config --list
-git config --system http.proxy http://proxy.intra.xacti-co.com:8080
-git config --system https.proxy https://proxy.intra.xacti-co.com:8080
+    :set_proxy
+    echo "In proxy, set proxy"
+    rem git config --list
+    git config --system http.proxy http://proxy.intra.xacti-co.com:8080
+    git config --system https.proxy https://proxy.intra.xacti-co.com:8080
 
-:end_gitconfig
+    :end_gitconfig
 
-mklink %HOMEPATH%"\.gitignore" ".\dotfiles\.gitignore"
+    mklink %HOMEPATH%"\.gitignore" ".\dotfiles\.gitignore"
 
-mklink %HOMEPATH%"\.vimrc" ".\dotfiles\.vimrc"
-mklink %HOMEPATH%"\.gvimrc" ".\dotfiles\.gvimrc"
-mklink /D %HOMEPATH%"\.vim" ".\dotfiles\.vim"
+    mklink %HOMEPATH%"\.vimrc" ".\dotfiles\.vimrc"
+    mklink %HOMEPATH%"\.gvimrc" ".\dotfiles\.gvimrc"
+    mklink /D %HOMEPATH%"\.vim" ".\dotfiles\.vim"
 
-echo End link
-pause
+    echo End set link
+    pause
 
-exit 0
+    exit 0
