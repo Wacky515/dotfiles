44925542ac55e8937233d8f6a59becd1914bd773
wacky515 <wacky515@gmail.com>
2016-12-28 18:02:58 +0900

    Update *.sh

--- Shell_install/x200MA_Ubuntu_install.sh
+++ Shell_install/x200MA_Ubuntu_install.sh
@@ -1,3 +1,4 @@
 wget http://download.teamviewer.com/download/teamviewer_i386.deb
 sudo dpkg -i teamviewer_i386.deb
-sudo dpkg -f install
+sudo apt-get -f install
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get autoremove && sudo apt-get autoclean && echo Update done x200MA || echo Error
