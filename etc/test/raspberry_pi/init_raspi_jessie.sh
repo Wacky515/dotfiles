043ec43502d250144c3a50bace246fc13e9a699d
wacky515 <wacky515@gmail.com>
2017-01-14 18:02:32 +0900

    Update init_raspi_jessie.sh

--- etc/test/raspberry_pi/init_raspi_jessie.sh
+++ etc/test/raspberry_pi/init_raspi_jessie.sh
@@ -42,50 +42,56 @@ install_package(){
 	# 仮想端末 インストール
 	# sudo apt-get install -y byobu
 
-    # TODO: no_lite
-    # 日本語環境 インストール
-    echo "Install Japanese environment"
-    # sudo apt install -y fonts-ipafont
-    # sudo apt install -y fonts-ipaexfont
-	sudo apt install -y fonts-vlgothic
-	sudo apt install -y ibus-mozc
-
-    # TODO: no_lite
-    # TightVNCViewer インストール
-    echo "Install \"TightVNCViewer\""
-	sudo apt install -y tightvncserver
-
-    # TODO: no_lite
-    # "man" コマンド日本語化
-    echo "Install japanese \"man\""
-    sudo apt install manpages-ja manpages-ja-dev
-
-    # TODO: no_lite
-    # "Chromium" インストール
-    echo "Install \"Chromium\""
-    wget -qO - \
-        http://bintray.com/user/downloadSubjectPublicKey?username=bintray \
-        | sudo apt-key add -
-    echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" \
-        | sudo tee -a /etc/apt/sources.list
-    sudo apt update
-    sudo apt install chromium-browser rpi-youtube -y
-    ginstall-ytdl
-
-    if [ "$?" -eq 0 ]
+    # "Lite" ではない時の処理
+    REV = `cat /proc/cmdline | /
+    awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+    if [ ! "$REV" -eq "900092" ]
     then
-        echo "Success install \"Chromium\""
-    else
-        echo "fail init install \"Chromium\""
+        # TODO: no_lite
+        # 日本語環境 インストール
+        echo "Install Japanese environment"
+        # sudo apt install -y fonts-ipafont
+        # sudo apt install -y fonts-ipaexfont
+        sudo apt install -y fonts-vlgothic
+        sudo apt install -y ibus-mozc
+
+        # TODO: no_lite
+        # TightVNCViewer インストール
+        echo "Install \"TightVNCViewer\""
+        sudo apt install -y tightvncserver
+
+        # TODO: no_lite
+        # "man" コマンド日本語化
+        echo "Install japanese \"man\""
+        sudo apt install manpages-ja manpages-ja-dev
+
+        # TODO: no_lite
+        # "Chromium" インストール
+        echo "Install \"Chromium\""
+        wget -qO - http://bintray.com/user/ \
+            downloadSubjectPublicKey?username=bintray \
+            | sudo apt-key add -
+        echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" \
+            | sudo tee -a /etc/apt/sources.list
+        sudo apt update
+        sudo apt install chromium-browser rpi-youtube -y
+        ginstall-ytdl
+
+        if [ "$?" -eq 0 ]
+        then
+            echo "Success install \"Chromium\""
+        else
+            echo "fail init install \"Chromium\""
+        fi
+
+        # TODO: no_lite
+        # 端末 インストール
+        # sudo apt-get install -y gnome-terminal
+
+        # TODO: no_lite
+        # パーティションツール インストール
+        # sudo apt-get install -y gparted
     fi
-
-    # TODO: no_lite
-	# 端末 インストール
-	# sudo apt-get install -y gnome-terminal
-
-    # TODO: no_lite
-	# パーティションツール インストール
-	# sudo apt-get install -y gparted
 }
 
 # 独自設定
@@ -132,29 +138,37 @@ setup_dotfiles(){
 	# sudo cp ./dotfiles/etc/test/raspberry_pi/.bashrc ~/.bashrc
 	# sudo cp ./dotfiles/etc/test/raspberry_pi/.zhrc ~/.zhrc
 
-    # TODO: no_lite
-    # "config.txt" 設定
-	sudo cp ./dotfiles/etc/test/raspberry_pi/config.txt /boot/config.txt
+    # "Lite" ではない時の処理
+    REV = `cat /proc/cmdline | /
+    awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+    if [ ! "$REV" -eq "900092" ]
+    then
+        # TODO: no_lite
+        # "config.txt" 設定
+        sudo cp ./dotfiles/etc/test/raspberry_pi/config.txt \
+            /boot/config.txt
 
-    # TODO: no_lite
-    # Wi-Fi 設定
-    sudo sh ./dotfiles/etc/test/raspberry_pi/setting_wifi.sh
+        # TODO: no_lite
+        # Wi-Fi 設定
+        sudo sh ./dotfiles/etc/test/raspberry_pi/setting_wifi.sh
 
-    # TODO: no_lite
-    # 仮想デスクトップ環境 設定
-	sudo cp ./dotfiles/etc/test/raspberry_pi/lxpolkit.desktop \
-        /etc/xdg/autostart/lxpolkit.desktop
+        # TODO: no_lite
+        # 仮想デスクトップ環境 設定
+        sudo cp ./dotfiles/etc/test/raspberry_pi/lxpolkit.desktop \
+            /etc/xdg/autostart/lxpolkit.desktop
 
-    # TODO: no_lite
-    # Automatically start up VNC
-	sudo cp ./dotfiles/etc/test/raspberry_pi/vncboot /etc/init.d/vncboot
-	sudo update-rc.d -f lightdm remove
-	sudo update-rc.d vncboot defaults
-
-
+        # TODO: no_lite
+        # Automatically start up VNC
+        sudo cp ./dotfiles/etc/test/raspberry_pi/vncboot \
+            /etc/init.d/vncboot
+        sudo update-rc.d -f lightdm remove
+        sudo update-rc.d vncboot defaults
+    else
     # TODO: lite
     # "config.txt" 設定
-	sudo cp ./dotfiles/etc/test/raspberry_pi/config_lite.txt /boot/config.txt
+	sudo cp ./dotfiles/etc/test/raspberry_pi/config_lite.txt \
+        /boot/config.txt
+    fi
 }
 
 START_TIME=`date +%s`
