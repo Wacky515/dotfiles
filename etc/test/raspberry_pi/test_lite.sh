02a8f9f68be74354745749c88fbd50f442ebeb13
wacky515 <wacky515@gmail.com>
2017-01-14 19:40:02 +0900

    Update init_raspi_jessie.sh

---
+++ etc/test/raspberry_pi/test_lite.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# "Lite" ではない時の処理
+REV=`cat /proc/cmdline | /
+awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+echo $REV
+# if [ ! "$REV" -eq "900092" ]
+# then
+#     # TODO: no_lite
+#     # 日本語環境 インストール
+#     echo "Install Japanese environment"
+#     # sudo apt install -y fonts-ipafont
+#     # sudo apt install -y fonts-ipaexfont
+#     sudo apt install -y fonts-vlgothic
+#     sudo apt install -y ibus-mozc
+#
+#     # TODO: no_lite
+#     # TightVNCViewer インストール
+#     echo "Install \"TightVNCViewer\""
+#     sudo apt install -y tightvncserver
+#
+#     # TODO: no_lite
+#     # "man" コマンド日本語化
+#     echo "Install japanese \"man\""
+#     sudo apt install manpages-ja manpages-ja-dev
+#
+#     # TODO: no_lite
+#     # "Chromium" インストール
+#     echo "Install \"Chromium\""
+#     wget -qO - http://bintray.com/user/ \
+#         downloadSubjectPublicKey?username=bintray \
+#         | sudo apt-key add -
+#     echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" \
+#         | sudo tee -a /etc/apt/sources.list
+#     sudo apt update
+#     sudo apt install chromium-browser rpi-youtube -y
+#     ginstall-ytdl
+#
+#     if [ "$?" -eq 0 ]
+#     then
+#         echo "Success install \"Chromium\""
+#     else
+#         echo "fail init install \"Chromium\""
+#     fi
+# echo ""
