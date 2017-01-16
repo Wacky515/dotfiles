bbaca1a36df66ed41057f75c8ddc5c613dc5f491
wacky515 <wacky515@gmail.com>
2017-01-16 15:57:17 +0900

    Update some files

--- etc/test/raspberry_pi/setting_hostname.sh
+++ etc/test/raspberry_pi/setting_hostname.sh
@@ -7,11 +7,13 @@ PROCESS="setting hostname"
 ym_echo "${PROCESS^}"
 
 # デフォルト設定 バックアップ
-sudo mv /etc/hostname /etc/hostname.bak
+# sudo mv /etc/hostname /etc/hostname.bak
+sudo cp /etc/hostname /etc/hostname.bak
 sudo cp /etc/hosts /etc/hosts.bak
 
 read newname
-sudo echo "$newname" > /etc/hostname
+# sudo echo "$newname" > /etc/hostname
+echo "$newname" | sudo tee /etc/hostname
 # sed -e "s/raspberrypi/"$newname"/g" /etc/hosts
 sudo sed -i "s/raspberrypi/"$newname"/g" /etc/hosts
 result_echo $? $PROCESS
