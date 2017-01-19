b13f1ac8e7279f05ec5075f5245d2c44fc9c4ec0
wacky515 <wacky515@gmail.com>
2017-01-20 08:56:02 +0900

    Update some files

--- etc/test/raspberry_pi/setting_wifi.sh
+++ etc/test/raspberry_pi/setting_wifi.sh
@@ -13,18 +13,8 @@ ym_echo ">> Input passphrase"
 read -sp "Passphrase: " PASPH
 echo ""
 
-# echo "SSID: ${ssid} Passphrase: ${pasph}"
 echo "SSID: ${SSID}"
 
 sudo sh -c "wpa_passphrase ${SSID} ${PASPH} >> \
     /etc/wpa_supplicant/wpa_supplicant.conf"
-
-# sudo sh -c "wpa_passphrase ${SSID} ${PASPH} >> test_wifi.txt"
-# sudo wpa_passphrase ${SSID} ${PASPH} >> \
-#     /etc/wpa_supplicant/wpa_supplicant.conf
-
-# stty -echo
-# wpa_passphrase ${SSID} ${PASPH} | sudo tee -a \
-#     /etc/wpa_supplicant/wpa_supplicant.conf
-# stty echo
 result_echo $? "${PROCESS}"
