6818d5fd29283e2f43ded573a66ef64f3b56454f
wacky515 <wacky515@gmail.com>
2017-01-21 17:45:18 +0900

    Update some files

--- etc/test/raspberry_pi/setting_keyboard.sh
+++ etc/test/raspberry_pi/setting_keyboard.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # @(#) This script is ~.
-source /home/pi/dotfiles/function/color_echo.sh
-source /home/pi/dotfiles/function/result_echo.sh
+# source /home/pi/dotfiles/function/color_echo.sh
+# source /home/pi/dotfiles/function/result_echo.sh
 
 # ファイル自身の絶対パス 取得
-readonly PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+# readonly PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 # source /../../../function/color_echo.sh
 # source /../../../function/result_echo.sh
 
 # source ../../../function/color_echo.sh
-# source ~/dotfiles/function/result_echo.sh
+source ~/dotfiles/function/result_echo.sh
 
 readonly PROCESS="setting keyboard layout"
 
 sudo mv /etc/default/keyboard /etc/default/keyboard.bak
-sudo cp ${PATH}/setting/keyboard \
-    /etc/default/
+# sudo cp ${PATH}/setting/keyboard \
+#     /etc/default/
 result_echo $? "${PROCESS}"
