224eb6d1a6ab550cd6cefc7fe3e068ffc1a9da85
wacky515 <wacky515@gmail.com>
2017-01-21 17:34:55 +0900

    Test setting_keyboard.sh

--- etc/test/raspberry_pi/setting_keyboard.sh
+++ etc/test/raspberry_pi/setting_keyboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # @(#) This script is ~.
-# source /home/pi/dotfiles/function/color_echo.sh
-# source /home/pi/dotfiles/function/result_echo.sh
+source /home/pi/dotfiles/function/color_echo.sh
+source /home/pi/dotfiles/function/result_echo.sh
 
 # ファイル自身の絶対パス 取得
 readonly PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
@@ -9,8 +9,8 @@ readonly PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 # source /../../../function/color_echo.sh
 # source /../../../function/result_echo.sh
 
-source ../../../function/color_echo.sh
-source ~/dotfiles/function/result_echo.sh
+# source ../../../function/color_echo.sh
+# source ~/dotfiles/function/result_echo.sh
 
 readonly PROCESS="setting keyboard layout"
 
