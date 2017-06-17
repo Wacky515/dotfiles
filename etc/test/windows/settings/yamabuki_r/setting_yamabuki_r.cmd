731c19b96d288371af116952bb8f2332ce34c376
wacky515 <wacky515@gmail.com>
2017-06-17 19:45:39 +0900

    Update setting windows files

--- etc/test/windows/settings/yamabuki_r/setting_yamabuki_r.cmd
+++ etc/test/windows/settings/yamabuki_r/setting_yamabuki_r.cmd
@@ -10,7 +10,21 @@ rem 設定ファイル コピー
 xcopy /y "Cust_NICOLA.yab" %HOMEPATH%"\yamabuki_r1.11.1"
 xcopy /y "yamabuki_r.ypr" %HOMEPATH%"\yamabuki_r1.11.1"
 
-rem スタートアップにショートカット 作成
-xcopy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+rem スタートアップにショートカット 作成(Windows7/10で当該Dir異なるはずやけど同じやがな・・・)
+ver | find "Version 6.1." > nul 
+if not errorlevel 1 goto win_7
 
+ver | find "Version 6.4." > nul 
+if not errorlevel 1 goto win_10
+
+:win_7
+echo "This PC is Windows7"
+xcopy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+goto end
+
+:win_10
+xcopy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+goto end
+
+:end
 pause
