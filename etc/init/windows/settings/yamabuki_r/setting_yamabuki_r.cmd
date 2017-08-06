24349d449924e156a0fe976802d2e65cb1014830
wacky515 <wacky515@gmail.com>
2017-08-06 21:59:28 +0900

    Update somefiles

--- etc/init/windows/settings/yamabuki_r/setting_yamabuki_r.cmd
+++ etc/init/windows/settings/yamabuki_r/setting_yamabuki_r.cmd
@@ -1,29 +1,32 @@
 @echo off
 
-rem "setting_yamabuki_r.cmd" がある "Dir" に "cd"
+rem スクリプトがある "Dir" に "cd"
 set bat_path=%~dp0
 cd /d %bat_path%
 
 echo "Setting Yamabuki R"
 
 rem 設定ファイル コピー
-copy /y "Cust_NICOLA.yab" %HOMEPATH%"\yamabuki_r1.11.1"
-copy /y "yamabuki_r.ypr" %HOMEPATH%"\yamabuki_r1.11.1"
+rem copy /y "Cust_NICOLA.yab" %HOMEPATH%"\yamabuki_r1.11.1"
+rem copy /y "yamabuki_r.ypr" %HOMEPATH%"\yamabuki_r1.11.1"
+
+copy /y "Cust_NICOLA.yab" %HOMEPATH%"\yamabuki_r1.11.1w"
+copy /y "yamabuki_r.ypr" %HOMEPATH%"\yamabuki_r1.11.1ws"
 
 rem スタートアップにショートカット 作成(Windows7/10で当該Dir異なるはずやけど同じやがな・・・)
 ver | find "Version 6.1." > nul
 if not errorlevel 1 goto win_7
 
-ver | find "Version 6.4." > nul
+ver | find "Version 10.0." > nul
 if not errorlevel 1 goto win_10
 
 :win_7
 echo "This PC is Windows7"
-xcopy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 goto end
 
 :win_10
-xcopy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "yamabuki_r.lnk" %HOMEPATH%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 goto end
 
 :end
