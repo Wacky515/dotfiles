8f8968c74509ffd0f52ecfc9091ff3ad4db38bca
wacky515 <wacky515@gmail.com>
2017-08-12 11:25:42 +0900

    Update some files

--- etc/init/windows/settings/chocolatey/init_and_update_chocolatey.cmd
+++ etc/init/windows/settings/chocolatey/init_and_update_chocolatey.cmd
@@ -1,22 +1,27 @@
 @echo off
 
-rem "setting_everything.cmd" がある "Dir" に "cd"
 set bat_path=%~dp0
+set config_files=packages_%computername%.config
+
+rem スクリプトがある "Dir" に "cd"
 cd /d %bat_path%
 
+rem "Chocolatey" インストール済みかチェック
 chocolatey -v >> nul
-rem echo %ERRORLEVEL%
 if %ERRORLEVEL% EQU 0 goto update
 
 echo "Install Chocolatey"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-rem TODO: 必須インストールソフト列挙テキスト（未制作）を読み込み、インストールする
-cinst packages.config
-
-rem ) else (
 
 :update
 echo "Already installed Chocolatey, Update software"
+
+rem DONE: "packages_***.config" を読み込み、インストール
+if exist %config_files% (
+    echo "Setting for this PC"
+    cinst -y %config_files%
+    )
+
 cup all -y
 
 rem デスクトップショートカット 作成
@@ -30,4 +35,5 @@ if not exist %USERPROFILE%"\Desktop\"%~n0".lnk" (
 :cplnk
 copy %~n0".lnk" %USERPROFILE%"\Desktop\"
 
+:end
 pause
