999183d966f07bdf5ced948b06ecbddc71a92626
wacky515 <wacky515@gmail.com>
2017-10-28 09:11:05 +0800

    Update somefiles

--- .vim/userautoload/vimgrep.vim
+++ .vim/userautoload/vimgrep.vim
@@ -1,4 +1,7 @@
 scriptencoding utf-8
 
-"vimgrep" 譎ゅ↓閾ｪ蜍慕噪縺ｫ "quick-fix"
+" "vimgrep" 譎ゅ↓閾ｪ蜍慕噪縺ｫ "quick-fix"
 autocmd QuickFixCmdPost *grep* cwindow
+
+" <Leader>G: カーソル下を "外部grep" 対象に挿入
+nnoremap <expr> <Leader>G ':sil grep! ' . expand('<cword>') . ' *'
--- link.cmd
+++ link.cmd
@@ -1,25 +1,26 @@
 @echo off
 
-rem ÉXÉNÉäÉvÉgÇ™Ç†ÇÈ "Dir" Ç… "cd"
+rem „Çπ„ÇØ„É™„Éó„Éà„Åå„ÅÇ„Çã "Dir" „Å´ "cd"
 cd /d %~dp0
 
 echo "Start set link"
 
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig"
 if %ERRORLEVEL% == 0 (
-        echo ".gitconfig link success!"
-        echo This PC name: %COMPUTERNAME%
-        for /f "delims=" %%i in (C:.\office_pc.txt) do (
-            echo In office PC: %%i
-            if /i %%i == %COMPUTERNAME% (
-                goto set_proxy
-                )
+    echo ".gitconfig link success!"
+
+    echo This PC name: %COMPUTERNAME%
+    for /f "delims=" %%i in (C:.\office_pc.txt) do (
+        echo In office PC: %%i
+        if /i %%i == %COMPUTERNAME% (
+            goto set_proxy
             )
-        echo "In normal network"
-            goto end_gitconfig
-        ) else (
-            goto end_gitconfig
         )
+    echo "In normal network"
+        goto end_gitconfig
+    ) else (
+        goto end_gitconfig
+    )
 
     :set_proxy
     echo "In proxy, set proxy"
@@ -27,9 +28,11 @@ if %ERRORLEVEL% == 0 (
     git config --system https.proxy https://proxy.intra.xacti-co.com:8080
 
     :end_gitconfig
+    rem "forï∂" Ç…Ç∑ÇÈ
     mklink %HOMEPATH%"\.minttyrc" ".\dotfiles\.minttyrc"
     mklink %HOMEPATH%"\.gitignore" ".\dotfiles\.gitignore"
     mklink %HOMEPATH%"\.zshrc" ".\dotfiles\.zshrc"
+    mklink %HOMEPATH%"\.ctags" ".\dotfiles\.ctags"
 
     mklink %HOMEPATH%"\.vimrc" ".\dotfiles\.vimrc"
     mklink %HOMEPATH%"\.gvimrc" ".\dotfiles\.gvimrc"
