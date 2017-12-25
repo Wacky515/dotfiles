2f4894b8aced9656ebf16f21e841d0ee6ea5524d
wacky515 <wacky515@gmail.com>
2017-12-25 21:05:00 +0900

    Update some files

--- etc/test/linux/ubuntu/install_tree.sh
+++ etc/test/linux/ubuntu/install_tree.sh
@@ -1,1 +1,10 @@
-sudo apt install tree
+#!/bin/bash
+# @(#) Intall tree
+
+source ~/dotfiles/function/result_echo.sh
+
+readonly PROCESS="install tree"
+
+ym_echo ">> ${PROCESS^}"
+sudo apt -y install tree
+result_echo $? "${PROCESS}"
