379a97d921c82e59bc96ee09fa0684e64d097391
wacky515 <wacky515@gmail.com>
2017-12-27 09:46:25 +0900

    Update some files

--- etc/test/linux/ubuntu/x32/install_teamviewer.sh
+++ etc/test/linux/ubuntu/x32/install_teamviewer.sh
@@ -11,12 +11,3 @@ wget http://download.teamviewer.com/download/teamviewer_i386.deb
 sudo dpkg -i teamviewer_i386.deb
 sudo apt-get -f install
 result_echo $? "${PROCESS}"
-
-# apt update
-# sudo ./apt_install.sh
-# sudo apt update && \
-# sudo apt -y upgrade && \
-# sudo apt -y dist-upgrade && \
-# sudo apt -y autoremove && \
-# sudo apt autoclean && \
-# echo "Update done $(hostname)" || echo "Error update $(hostname)"
