41fbc340909fedd8575ce04edb60bf6de3ab25e8
wacky515 <wacky515@gmail.com>
2017-12-27 09:46:25 +0900

    Update some files

--- etc/test/linux/install_zsh.sh
+++ etc/test/linux/install_zsh.sh
@@ -7,6 +7,6 @@ readonly PROCESS="install zsh"
 
 ym_echo ">> ${PROCESS^}"
 sudo apt -y install zsh
+result_echo $? "${PROCESS}"
 ../raspberry_pi/fix_zsh.sh
 source ~/.zshrc
-result_echo $? "${PROCESS}"
