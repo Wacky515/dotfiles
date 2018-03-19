39334f3d2d77f83770e2b8ea33c88dd0951c0195
wacky515 <wacky515@gmail.com>
2018-03-19 09:20:50 +0800

    Update some files

--- link.cmd
+++ link.cmd
@@ -1,5 +1,6 @@
 @echo off
-rem Last Change: 2018/03/18 22:10:27.
+rem Last Change: 2018/03/19 09:20:37.
+rem TODO: "init.vim" のシンボリックリンクソースのパスが変数化できない
 
 rem  スクリプトがある "Dir" に "cd"
 cd /d %~dp0
@@ -9,31 +10,32 @@ echo ^>^> Start set link
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig" > nul 2>&1
 if %ERRORLEVEL% == 0 (
     echo .gitconfig link success!
+    rem echo This PC name: %COMPUTERNAME%{{{
+    rem for /f "delims=" %%i in (C:.\murata_pc.txt) do (
+    rem rem for /f "delims=" %%i in (C:.\xacti_pc.txt) do (
+    rem     echo In office PC: %%i
+    rem     if /i %%i == %COMPUTERNAME% (
+    rem         goto set_proxy
+    rem         )
+    rem     )
+    rem echo "In normal network"
+    rem     goto mk_sylink
+    rem ) else (
+    rem     goto mk_sylink
+    rem )
 
-    echo This PC name: %COMPUTERNAME%
-    for /f "delims=" %%i in (C:.\murata_pc.txt) do (
-    rem for /f "delims=" %%i in (C:.\xacti_pc.txt) do (
-        echo In office PC: %%i
-        if /i %%i == %COMPUTERNAME% (
-            goto set_proxy
-            )
-        )
-    echo "In normal network"
-        goto mk_sylink
-    ) else (
-        goto mk_sylink
-    )
+rem :set_proxy
+rem echo "In proxy, set proxy"
 
-:set_proxy
-echo "In proxy, set proxy"
-
-git config --system http.proxy http://m6prxy1:8080
-git config --system https.proxy https://m6prxy1:8080
+rem git config --system http.proxy http://m6prxy1:8080
+rem git config --system https.proxy https://m6prxy1:8080
 rem git config --system http.proxy http://proxy.intra.xacti-co.com:8080 rem {{{
 rem git config --system https.proxy https://proxy.intra.xacti-co.com:8080
 rem }}}
 
-:mk_sylink
+rem :mk_sylink
+/*}}}*/
+
 for %%j in (.*) do (
     if %%j == .bash_history (
     rem echo ignore1 %%j
--- link.sh
+++ link.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # @(#) Setting keyboard layout.
+# Last Change: 2018/03/19 09:17:00.
 # TODO:
     # ".vim" ディレクトリのシンボリックリンクを "NeoVim" 用にする
+    # "Git" の "proxy" の設定は切替え用のシェルスクリプトに移管
     # リンクの "path" を全体的に変えたので確認
 # FIXME:
     # ${HOME} を単体起動と外部起動で通常動作させる
-# Last Chqange: .
 
 # DONE:
     # 暫定的にコマンドを決め打ちにした
@@ -29,30 +30,30 @@ ln -snfv ${DOT_DIR}/.gitconfig ~/.gitconfig
 if [ $? = 0 ]
 then
     ym_echo ">> .gitconfig link success!"
-    echo ""
-
-    readonly PC_NAME=${HOSTNAME}
-    readonly PROXY_PC=($(cat ./office_pc.txt));
-    readonly IFS_SAVE=${IFS}
-    readonly IFS=$'\n'
-
-    ym_echo ">> This PC name: "${PC_NAME}
-    ym_echo ">> In office PCs: "
-    ym_echo ${PROXY_PC[@]}
-
-    if ! $(echo ${PROXY_PC[@]} | grep -q "$PC_NAME")
-    then
-        ym_echo ">> In normal network"
-    else
-        ym_echo ">> In proxy network, set proxy"
-        ym_echo ">> Enter password to "su" command(2times)"
-        su -c "git config --system http.proxy \
-            http.proxy http://m6prxy1:8080"
-            # http://proxy.intra.xacti-co.com:8080"
-        su -c "git config --system https.proxy \
-            https.proxy http://m6prxy1:8080"
-            # https://proxy.intra.xacti-co.com:8080"
-    fi
+    # echo "" # {{{
+    # readonly PC_NAME=${HOSTNAME}
+    # readonly PROXY_PC=($(cat ./office_pc.txt));
+    # readonly IFS_SAVE=${IFS}
+    # readonly IFS=$'\n'
+    #
+    # ym_echo ">> This PC name: "${PC_NAME}
+    # ym_echo ">> In office PCs: "
+    # ym_echo ${PROXY_PC[@]}
+    #
+    # if ! $(echo ${PROXY_PC[@]} | grep -q "$PC_NAME")
+    # then
+    #     ym_echo ">> In normal network"
+    # else
+    #     ym_echo ">> In proxy network, set proxy"
+    #     ym_echo ">> Enter password to "su" command(2times)"
+    #     su -c "git config --system http.proxy \
+    #         http.proxy http://m6prxy1:8080"
+    #         # http://proxy.intra.xacti-co.com:8080"
+    #     su -c "git config --system https.proxy \
+    #         https.proxy http://m6prxy1:8080"
+    #         # https://proxy.intra.xacti-co.com:8080"
+    # fi
+# }}}
 fi
 echo ""
 
@@ -69,29 +70,27 @@ do
     # 無視したいファイルやディレクトリは以下に追記
 
     # ".bash_history" の "Init処理" を追加（未テスト）
-    # ".zsh_history" の "Init処理" を追加（未テスト）
     [[ ${f} = ".bash_history" ]] cp -snv ${DOR_DIR}/${f} ~/${f}
+    # [[ ${f} = ".bash_history" ]] && continue
+
+    # ".zsh_history" の "Init処理" を追加（未テスト）
     [[ ${f} = ".zsh_history" ]] cp -snv ${DOR_DIR}/${f} ~/${f}
-#     # [[ ${f} = ".bash_history" ]] && continue
-#     # [[ ${f} = ".zsh_history" ]] && continue
+    # [[ ${f} = ".zsh_history" ]] && continue
 
     [[ ${f} = ".git" ]] && continue
     [[ ${f} = ".git" ]] && continue
-    # [[ ${f} = ".gitignore" ]] && continue
     [[ ${f} = ".gitconfig" ]] && continue
-
-    [[ ${f} = "README.md" ]] && continue
+    # [[ ${f} = ".gitignore" ]] && continue
 
     [[ ${f} == ".DS_Store" ]] && continue
-
-    [[ ${f} = "*.sh" ]] && continue
-    [[ ${f} = ".modfying" ]] && continue
-
     [[ ${f} = "*. ~" ]] && continue
     [[ ${f} = "*.un~" ]] && continue
     [[ ${f} = "*.swp" ]] && continue
-
     [[ ${f} = "*.old" ]] && continue
+
+    [[ ${f} = "README.md" ]] && continue
+    [[ ${f} = "*.sh" ]] && continue
+    [[ ${f} = ".modfying" ]] && continue
 
     # シンボリックリンク 作成
     ym_echo ${f}
@@ -101,4 +100,4 @@ ln -snfv ~/.vim ~/.config/nvim/
 
 ym_echo ">> dotfiles link success"
 ym_echo ">> End make symbolic link"
-ym_echo ""
+echo ""
