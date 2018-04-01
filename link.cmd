07c867a6b792eab403aa24539d05cedd98faa5f2
wacky515 <wacky515@gmail.com>
2018-04-01 20:50:31 +0900

    Add nyaovim link(has error)

--- link.cmd
+++ link.cmd
@@ -1,6 +1,7 @@
 @echo off
-rem Last Change: 2018/03/31 22:45:19.
+rem Last Change: 2018/04/01 20:49:54.
 
+rem FIXME: ginit.vim コピー後、)の使い方が誤っています エラーになる
 rem TODO: 常時管理者として実行する
 
 rem  スクリプトがある "Dir" に "cd"
@@ -12,15 +13,28 @@ echo ^>^> Start set link
 rem "NeoVim" 設定
 rem TODO: "init.vim"、"ginit.vim" シンボリックリンクの
 rem ソースのパスが変数化できない為、"copy"で暫定対応
+rem %HOMEPATH%\AppData\Local の nvim をシンボリックリンクにしてはどうか？
 copy init.vim %NVIM_PATH%
 if %ERRORLEVEL% == 0 (
-    echo init.vim link success!
+    echo init.vim copy success!
     )
 copy ginit.vim %NVIM_PATH%
 if %ERRORLEVEL% == 0 (
-    echo ginit.vim link success!
+    echo ginit.vim copy success!
+    )
+rem FTR:
+rem mklink /D %HOMEPATH%"\AppData\Local\nvim" ".\dotfiles\nvim" > nul 2>&1
+rem if %ERRORLEVEL% == 0 (
+rem     echo .vim link success!
+rem     )
+
+rem "NyaoVim" 設定
+mklink %HOMEPATH%"\AppData\Roaming\NyaoVim\nyaovimrc.html" ".\dotfiles\nyaovimrc.html" > nul 2>&1
+if %ERRORLEVEL% == 0 (
+    echo nyaovimrc.html link success!
     )
 
+rem  "dotfiles" 設定
 mklink %HOMEPATH%"\.gitconfig" ".\dotfiles\.gitconfig" > nul 2>&1
 if %ERRORLEVEL% == 0 (
     echo .gitconfig link success!
