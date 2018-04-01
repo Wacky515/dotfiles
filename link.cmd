6fb059e1b337d0d32fccb2979a6a6664a388c37d
wacky515 <wacky515@gmail.com>
2018-04-02 08:58:13 +0900

    Fix miss if

--- link.cmd
+++ link.cmd
@@ -1,8 +1,11 @@
 @echo off
-rem Last Change: 2018/04/01 20:49:54.
+rem Last Change: 2018/04/02 08:56:50.
 
-rem FIXME: ginit.vim コピー後、)の使い方が誤っています エラーになる
 rem TODO: 常時管理者として実行する
+rem TODO: "init.vim"、"ginit.vim" シンボリックリンクの
+rem ソースのパスが変数化できない為、"copy"で暫定対応
+
+rem DONE: ginit.vim コピー後、)の使い方が誤っています エラーになる
 
 rem  スクリプトがある "Dir" に "cd"
 cd /d %~dp0
@@ -11,27 +14,31 @@ set NVIM_PATH=%HOMEPATH%"\AppData\Local\nvim"
 echo ^>^> Start set link
 
 rem "NeoVim" 設定
-rem TODO: "init.vim"、"ginit.vim" シンボリックリンクの
-rem ソースのパスが変数化できない為、"copy"で暫定対応
-rem %HOMEPATH%\AppData\Local の nvim をシンボリックリンクにしてはどうか？
-copy init.vim %NVIM_PATH%
-if %ERRORLEVEL% == 0 (
-    echo init.vim copy success!
+rem NG: %HOMEPATH%\AppData\Local の nvim をシンボリックリンクにしてはどうか？
+
+if exist %NVIM_PATH% (
+    copy init.vim %NVIM_PATH%
+    if %ERRORLEVEL% == 0 (
+        echo init.vim copy success!
+        )
+    copy ginit.vim %NVIM_PATH%
+    if %ERRORLEVEL% == 0 (
+        echo ginit.vim copy success!
+        )
     )
-copy ginit.vim %NVIM_PATH%
-if %ERRORLEVEL% == 0 (
-    echo ginit.vim copy success!
-    )
-rem FTR:
+rem NG: rem {{{
 rem mklink /D %HOMEPATH%"\AppData\Local\nvim" ".\dotfiles\nvim" > nul 2>&1
 rem if %ERRORLEVEL% == 0 (
-rem     echo .vim link success!
-rem     )
+rem     echo nvim link success!
+rem )
+rem }}}
 
 rem "NyaoVim" 設定
-mklink %HOMEPATH%"\AppData\Roaming\NyaoVim\nyaovimrc.html" ".\dotfiles\nyaovimrc.html" > nul 2>&1
-if %ERRORLEVEL% == 0 (
-    echo nyaovimrc.html link success!
+if exist %HOMEPATH%"\AppData\Roaming\NyaoVim" (
+    mklink %HOMEPATH%"\AppData\Roaming\NyaoVim\nyaovimrc.html" ".\dotfiles\nyaovimrc.html" > nul 2>&1
+    if %ERRORLEVEL% == 0 (
+        echo nyaovimrc.html link success!
+        )
     )
 
 rem  "dotfiles" 設定
@@ -41,22 +48,25 @@ if %ERRORLEVEL% == 0 (
     )
 
 rem :mk_sylink
-for %%j in (.*) do (
-    if %%j == .bash_history (
-    ) else if %%j == .gitconfig (
-    ) else if %%j == .zsh_history (
-    ) else (
-        mklink %HOMEPATH%"\"%%j ".\dotfiles\"%%j > nul 2>&1
-        if %ERRORLEVEL% == 0 (
-            echo %%j link success!
-            )
-        )
-    )
-
 mklink /D %HOMEPATH%"\.vim" ".\dotfiles\.vim" > nul 2>&1
 if %ERRORLEVEL% == 0 (
     echo .vim link success!
     )
+
+for %%j in (.*) do (
+    if %%j == .bash_history (
+    rem echo ignore1 %%j :消すな！
+    ) else if %%j == .gitconfig (
+    rem echo ignore2 %%j :消すな！
+    ) else if %%j == .zsh_history (
+    rem echo ignore3 %%j :消すな！
+    ) else (
+    mklink %HOMEPATH%"\"%%j ".\dotfiles\"%%j > nul 2>&1
+    if %ERRORLEVEL% == 0 (
+        echo Set link %%j
+        )
+    )
+)
 
 echo ^>^> End set link
 
