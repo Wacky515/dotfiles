05d1d5a7e03104e10c47a96b9e04797756f400c8
MME2M4 脇田　純 <MM12167@dmj.murata.co.jp>
2018-04-13 09:58:26 +0900

    Add Shell script template

---
+++ .vim/template/template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @(#) ***
+# Created:     __DATE__
+# Last Change: 2018/04/13 09:57:23.
+
+source ~/dotfiles/function/result_echo.sh
+source ~/dotfiles/function/color_echo.sh
+
+ym_echo ">> Start "
