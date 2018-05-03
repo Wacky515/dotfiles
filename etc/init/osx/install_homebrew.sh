7dc7a4f32556793893f7b68d2380e22f17ef4242
wacky515 <wacky515@gmail.com>
2018-05-03 11:51:07 +0900

    Add install HomeBrew

---
+++ etc/init/osx/install_homebrew.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# @(#) Install HomeBrew
+# Created:     2018/05/03 10:58:45
+# Last Change: 2018/05/03 11:39:59.
+
+source ~/dotfiles/function/result_echo.sh
+source ~/dotfiles/function/color_echo.sh
+
+ym_echo ">> Start check HomeBrew install or not"
+
+brew --version
+
+if [ $? != 0 ]
+then
+ym_echo ">> Installed brew"
+
+fi
+ym_echo ">> Install brew"
+
+xcode-select --install
+xcode-select: note: install requested for command line developer tools
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"k
+
+brew tap caskroom/cask
+brew tap
+brew tap cskroom/versions
+
+brew doctor
