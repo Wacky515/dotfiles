53b226f923b0119526dffee61b891105b82668fe
wacky515 <wacky515@gmail.com>
2018-05-07 07:25:48 +0900

    Fix directory layout

--- etc/init/linux/install_cica.sh
+++ etc/init/linux/install_cica.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # @(#) Install Cica font for Ubuntu
 # Created:     2018/05/06 18:08:09
-# Last Change: 2018/05/07 07:04:57.
+# Last Change: 2018/05/07 07:24:55.
 
 source ~/dotfiles/function/result_echo.sh
 source ~/dotfiles/function/color_echo.sh
@@ -14,19 +14,21 @@ cd ~/
 sudo apt-get -y install fontforge unar
 git clone git@github.com:miiton/Cica.git
 wget -O ubuntu-font-family-0.83.zip https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip
-mkdir ./sourceFonts/
+
+mkdir ~/sourceFonts/
 unar ubuntu-font-family-0.83.zip
-cp ubuntu-font-family-0.83/UbuntuMono-R.ttf ./sourceFonts/
-cp ubuntu-font-family-0.83/UbuntuMono-B.ttf ./sourceFonts/
+cp ubuntu-font-family-0.83/UbuntuMono-R.ttf ~/sourceFonts/
+cp ubuntu-font-family-0.83/UbuntuMono-B.ttf ~/sourceFonts/
+
 wget https://osdn.jp/downloads/users/8/8598/rounded-mgenplus-20150602.7z
 unar rounded-mgenplus-20150602.7z
-cp rounded-mgenplus-20150602/rounded-mgenplus-1m-regular.ttf ./sourceFonts/
-cp rounded-mgenplus-20150602/rounded-mgenplus-1m-bold.ttf ./sourceFonts/
-wget https://github.com/googlei18n/noto-emoji/raw/master/fonts/NotoEmoji-Regular.ttf -O sourceFonts/NotoEmoji-Regular.ttf
+cp rounded-mgenplus-20150602/rounded-mgenplus-1m-regular.ttf ~/sourceFonts/
+cp rounded-mgenplus-20150602/rounded-mgenplus-1m-bold.ttf ~/sourceFonts/
+wget https://github.com/googlei18n/noto-emoji/raw/master/fonts/NotoEmoji-Regular.ttf -O ~/sourceFonts/NotoEmoji-Regular.ttf
 fontforge -lang=py -script cica.py
 
-rm -ri rounded-mgenplus-20150602/
-rm -ri rounded-mgenplus-20150602.7zz
-rm -ri sourceFonts/
-rm -ri ubuntu-font-family-0.83/
-rm -ri ubuntu-font-family-0.83.zip
+rm -ri ~/sourceFonts/
+rm -ri ~/rounded-mgenplus-20150602/
+rm -ri ~/rounded-mgenplus-20150602.7z
+rm -ri ~/ubuntu-font-family-0.83/
+rm -ri ~/ubuntu-font-family-0.83.zip
