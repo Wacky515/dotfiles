bb5487e2e9b3c8f50b84fdfdb45483d6b6564c4b
wacky515 <wacky515@gmail.com>
2018-05-09 07:09:29 +0900

    Update some files

--- etc/init/linux/NY_install_cica.sh
+++ etc/init/linux/NY_install_cica.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # @(#) Install Cica font for Ubuntu
 # Created:     2018/05/06 18:08:09
-# Last Change: 2018/05/07 07:24:55.
+# Last Change: 2018/05/09 06:43:47.
+
+# FIXME:
+# fontforge: can't open file 'cica.py': [Errno 2] そのようなファイルやディレクトリはありません
 
 source ~/dotfiles/function/result_echo.sh
 source ~/dotfiles/function/color_echo.sh
 
 ym_echo ">> Start install Cica font"
 
-sudo apt-get update
+sudo apt update
 cd ~/
 
-sudo apt-get -y install fontforge unar
+sudo apt -y install fontforge unar
 git clone git@github.com:miiton/Cica.git
 wget -O ubuntu-font-family-0.83.zip https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip
 
@@ -27,8 +30,8 @@ cp rounded-mgenplus-20150602/rounded-mgenplus-1m-bold.ttf ~/sourceFonts/
 wget https://github.com/googlei18n/noto-emoji/raw/master/fonts/NotoEmoji-Regular.ttf -O ~/sourceFonts/NotoEmoji-Regular.ttf
 fontforge -lang=py -script cica.py
 
-rm -ri ~/sourceFonts/
-rm -ri ~/rounded-mgenplus-20150602/
-rm -ri ~/rounded-mgenplus-20150602.7z
-rm -ri ~/ubuntu-font-family-0.83/
-rm -ri ~/ubuntu-font-family-0.83.zip
+rm -r ~/sourceFonts/
+rm -r ~/rounded-mgenplus-20150602/
+rm -r ~/rounded-mgenplus-20150602.7z
+rm -r ~/ubuntu-font-family-0.83/
+rm -r ~/ubuntu-font-family-0.83.zip
