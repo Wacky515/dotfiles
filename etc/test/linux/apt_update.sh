14feb9de06080210880724a8ed77c989dd47b2b1
wacky515 <wacky515@gmail.com>
2018-05-12 08:39:43 +0900

    Add comment

--- etc/test/linux/apt_update.sh
+++ etc/test/linux/apt_update.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# @(#) Apt update
+# @(#) apt update
+# Created:     2017/12/25 00:00:00
+# Last Change: 2018/05/10 18:30:41.
 
 source ~/dotfiles/function/result_echo.sh
+source ~/dotfiles/function/color_echo.sh
 
-readonly PROCESS="Apt update"
+readonly PROCESS="apt update"
 
-# apt update
-# echo "Update $(hostname)"
 ym_echo ">> ${PROCESS^}"
+
 sudo apt update && \
 sudo apt -y upgrade && \
 sudo apt -y dist-upgrade && \
 sudo apt -y autoremove && \
+
 # sudo apt autoclean && \
 # echo "Update done $(hostname)" || echo "Error update $(hostname)"
 sudo apt autoclean
+
 result_echo $? "${PROCESS}"
