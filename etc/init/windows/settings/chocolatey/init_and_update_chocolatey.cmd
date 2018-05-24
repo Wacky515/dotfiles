84cb213f9ce7d09b12672c8eb585c9b7de538c49
wacky515 <wacky515@gmail.com>
2018-05-24 22:16:45 +0800

    Update some files

--- etc/init/windows/settings/chocolatey/init_and_update_chocolatey.cmd
+++ etc/init/windows/settings/chocolatey/init_and_update_chocolatey.cmd
@@ -1,4 +1,6 @@
 @echo off
+rem Created:     2017/02/17 00:54:41
+rem Last Change: 2018/05/24 22:16:21.
 
 rem http://pachicoma.hateblo.jp/entry/2017/02/28/Chocolatey%E3%81%A8%E3%81%84%E3%81%86Windows10%E3%83%91%E3%83%83%E3%82%B1%E3%83%BC%E3%82%B8%E3%83%9E%E3%83%8D%E3%83%BC%E3%82%B8%E3%83%A3%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B
 rem 上記URL参照してリファクタリング
@@ -13,15 +15,15 @@ rem "Chocolatey" インストール済みかチェック
 chocolatey -v >> nul
 if %ERRORLEVEL% EQU 0 goto update
 
-echo "Install Chocolatey"
+echo ^>^> Install Chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 :update
-echo "Already installed Chocolatey, Update software"
+echo ^>^> Already installed Chocolatey, Update software
 
 rem DONE: "packages_***.config" を読み込み、インストール
 if exist %config_files% (
-    echo "Setting for this PC"
+    echo ^>^> Setting for this PC
     cinst -y %config_files%
     )
 
@@ -31,7 +33,7 @@ rem デスクトップショートカット 作成
 if not exist %USERPROFILE%"\Desktop\"%~n0".lnk" (
     goto cplnk
 ) else (
-    echo "Already set desktop shortcut"
+    echo ^>^> Already set desktop shortcut
     goto end
 )
 
@@ -40,3 +42,4 @@ copy %~n0".lnk" %USERPROFILE%"\Desktop\"
 
 :end
 rem pause
+exit /b 0
