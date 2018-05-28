b82190dfaf92caef1f280cf6b5b22ff4f96f1fc8
wacky515 <wacky515@gmail.com>
2018-05-28 10:07:36 +0800

    Update some files

--- etc/test/osx/init_osx.sh
+++ etc/test/osx/init_osx.sh
@@ -1,32 +1,32 @@
 #!bin/bash
 # @(#) Initialize OS X
 # Created:     2018/05/21 16:55:15
-# Last Change: 2018/05/28 09:53:30.
+# Last Change: 2018/05/28 10:07:20.
 
 source ~/dotfiles/function/result_echo.sh
 source ~/dotfiles/function/color_echo.sh
-
 readonly PROCESS="init OSX"
-gm_echo ">> ${PROCESS^}"
+
+gm_echo ">> ${PROCESS^}" || gm_echo ">> ${PROCESS}"
 
 # Make symbolic link
-gm_echogc ">> Link setting files"
-gm_echogc ">> Call symbolic linking script"
+gm_echo ">> Link setting files"
+gm_echo ">> Call symbolic linking script"
 sh ~/dotfiles/link.sh
 # bash ~/dotfiles/link.sh
 
 cd ~/dotfiles/etc/init/osx
 
-gm_echogc ">> Istall Homebrew"
+gm_echo ">> Istall Homebrew"
 sudo bash ./install_homebrew.sh
 
-gm_echogc ">> Init brwe install"
+gm_echo ">> Init brwe install"
 for f in brew_*.sh
 do
     sudo bash ./${f}
 done
 
-gm_echogc ">> Init setting"
+gm_echo ">> Init setting"
 for g in setting_*.sh
 do
     sudo bash ./${g}
@@ -34,13 +34,13 @@ done
 
 cd ~/dotfiles/etc/test/osx
 
-gm_echogc ">> Init brew install(Test)"
+gm_echo ">> Init brew install(Test)"
 for f in brew_*.sh
 do
     sudo bash ./${f}
 done
 
-gm_echogc ">> Init setting(Test)"
+gm_echo ">> Init setting(Test)"
 for g in setting_*.sh
 do
     sudo bash ./${g}
