d57372f4a316a18b6f8aae9ce8a4cfc503458324
wacky515 <wacky515@gmail.com>
2018-05-28 12:01:19 +0800

    Update some files

--- etc/test/linux/make_dotfiles.sh
+++ etc/test/linux/make_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # @(#) Initial install dotfiles
 # Created:     2018/05/09 10:15:36
-# Last Change: 2018/05/28 11:57:26.
+# Last Change: 2018/05/28 12:00:47.
 
 # FIXME: OS X: echoの文頭名のファイルが生成されてしまう
 
@@ -179,11 +179,11 @@ darwin*)
         info "Brew install Bash4.x"
         # brew install bash
         brew upgrade bash > /dev/null 2>&1
+        # sudo ln -s /usr/local/bin/bash /bin/bash
+        sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
+        chsh -s /usr/local/bin/bash
+        # exec $SHELL -l
     fi
-    # sudo ln -s /usr/local/bin/bash /bin/bash
-    sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
-    chsh -s /usr/local/bin/bash
-    exec $SHELL -l
     info "Call setting OS X"
     sh ~/dotfiles/etc/test/osx/init_osx.sh
     ;;
