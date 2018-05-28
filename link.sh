bb66df4cdcbf2e28cc43f2a30b8728726ab492ee
wacky515 <wacky515@gmail.com>
2018-05-28 15:39:15 +0800

    Update some files

--- link.sh
+++ link.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # @(#) Symbolic linkig dotfiles.
 # Created:     2017/02/08 00:00:00
-# Last Change: 2018/05/28 15:30:22.
-gm_echo ">> ${PROCESS}"
+# Last Change: 2018/05/28 15:37:26.
 # TODO:
 # FIXME:
     # ${HOME} を単体起動と外部呼出しで通常動作させる
@@ -29,15 +28,16 @@ cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 readonly DOT_DIR="${HOME}/dotfiles"
 readonly PROCESS="symbolic linkig dotfiles"
 
-case ${OSTYPE} in
-darwin*)
-    gm_echo ">> ${PROCESS}"
-    gm_echo ">> ${PROCESS^}"
-    ;;
-linux*)
-    gm_echo ">> ${PROCESS^}"
-    ;;
-esac
+gm_echo ">> ${PROCESS^}"
+
+# case ${OSTYPE} in
+# darwin*)
+#     gm_echo ">> ${PROCESS}"
+#     ;;
+# linux*)
+#     gm_echo ">> ${PROCESS^}"
+#     ;;
+# esac
 
 gm_echo ">> Start .gitconfig link"
 
