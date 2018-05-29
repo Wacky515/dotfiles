1f361a235339f03e8204cb8f1e3cc38388eba34c
wacky515 <wacky515@gmail.com>
2018-05-29 08:17:05 +0800

    Add information

--- etc/init/osx/setting_dock.sh
+++ etc/init/osx/setting_dock.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# @(#) Setting Dock
+# Created:     2018/05/26 00:00:00
+# Last Change: 2018/05/29 08:09:35.
+
+for f in ~/dotfiles/function/*.sh
+do
+    source ${f}
+done
+
+readonly PROCESS="setting Dock"
+
+gm_echo ">> ${PROCESS^}"
+
 defaults write com.apple.dock magnification -bool true
+
+result_echo $? "${PROCESS}"
