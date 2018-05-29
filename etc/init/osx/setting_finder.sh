97d4c8687b0a64aaf67d049b78b20c00865f2b9f
wacky515 <wacky515@gmail.com>
2018-05-29 08:17:05 +0800

    Add information

--- etc/init/osx/setting_finder.sh
+++ etc/init/osx/setting_finder.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+# @(#) Setting Finder
+# Created:     2018/05/26 00:00:00
+# Last Change: 2018/05/29 08:15:04.
+
+for f in ~/dotfiles/function/*.sh
+do
+    source ${f}
+done
+
+readonly PROCESS="setting Finder"
+
+gm_echo ">> ${PROCESS^}"
+
 defaults write com.apple.finder AppleShowAllFiles -boolean true
 killall Finder
+
+result_echo $? "${PROCESS}"
