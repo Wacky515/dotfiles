43540c2b51ec0b4a19ae059acdcbf36533514fb2
MME2M4 脇田　純 <MM12167@dmj.murata.co.jp>
2018-06-03 20:08:28 +0800

    Fix install HomeBrew

--- etc/init/osx/install_homebrew.sh
+++ etc/init/osx/install_homebrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @(#) Install HomeBrew
 # Created:     2018/05/03 10:58:45
-# Last Change: 2018/06/03 19:56:26.
+# Last Change: 2018/06/03 20:08:03.
 
 for f in ~/dotfiles/function/*.sh
 do
@@ -24,25 +24,32 @@ else
     xcode-select: note: install requested for command line developer tools
 
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"k
+fi
 
+if ! has "brew tap"; then
     brew tap caskroom/cask
     brew tap
     brew tap cskroom/versions
 
     brew doctor
+fi
 
+if ! has "mas"; then
     brew install argon/mas/mas
     brew install rcmdnk/file/brew-file
+fi
 
+if ! has "Brewfile"; then
     brew file install Brewfile
     # cp ./brewfile ~/.config
     cp ~/dotfiles/etc/init/osx/brewfile/Brewfile ~/.config
     brew file install Brewfile
-
-    bash ./brew_update.sh
 fi
 
+bash ./brew_update.sh
+
 warn "ここからエラー！！！"
+
 brew file install Brewfile
 cp ~/dotfiles/etc/init/osx/brewfile/Brewfile ~/.config
 brew file install Brewfile
