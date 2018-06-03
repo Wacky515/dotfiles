54ed2c13865907aed17a7063e606519f11af87d3
MME2M4 脇田　純 <MM12167@dmj.murata.co.jp>
2018-06-03 21:59:00 +0800

    Update

--- etc/init/osx/install_homebrew.sh
+++ etc/init/osx/install_homebrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @(#) Install HomeBrew
 # Created:     2018/05/03 10:58:45
-# Last Change: 2018/06/03 21:56:45.
+# Last Change: 2018/06/03 21:58:32.
 
 for f in ~/dotfiles/function/*.sh
 do
@@ -53,7 +53,7 @@ fi
 #     gm_echo ">> Install brew-file"
     brew install rcmdnk/file/brew-file
     brew file install Brewfile
-        ln ~/dotfiles/etc/init/osx/brewfile/Brewfile \
+        ln -s ~/dotfiles/etc/init/osx/brewfile/Brewfile \
             ~/.config/brewfile
     brew file install Brewfile
 # else
