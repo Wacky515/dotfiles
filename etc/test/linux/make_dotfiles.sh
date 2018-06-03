b4291e50130f16b4b082b161e57684bb103d4421
wacky515 <wacky515@gmail.com>
2018-06-03 18:44:33 +0800

    Update

--- etc/test/linux/make_dotfiles.sh
+++ etc/test/linux/make_dotfiles.sh
@@ -1,25 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # @(#) Initial install dotfiles
 # Created:     2018/05/09 10:15:36
-# Last Change: 2018/05/28 20:01:43.
+# Last Change: 2018/06/03 18:40:10.
 
 # FIXME:
+    # sudo権限を終了しないとbrewが使えない
 
-# DONE: "type" を "has" に置き換える
-# DONE: OS X: echoの文頭名のファイルが生成されてしまう
+# DONE:
+    # "type" を "has" に置き換える
+    # OS X: echoの文頭名のファイルが生成されてしまう
     # ワンライナーの末尾 "~ | bash)" が原因
 
 set -euo pipefail
-
-# source ~/dotfiles/function/result_echo.sh /dev/null 2>&1
-# source ~/dotfiles/function/color_echo.sh /dev/null 2>&1
+export LC_ALL=C
 
 readonly PROCESS="make dotfiles"
 
-readonly DOT_DIRECTORY="${HOME}/dotfiles"
+readonly DOT_DIR="${HOME}/dotfiles"
 readonly GIT_URL="https://github.com/Wacky515/dotfiles.git"
-
-# ym_echo ">> ${PROCESS^}" /dev/null 2>&1 || echo ">> ${PROCESS^}"
 
 ### 設定
 # 色付きのテキストを端末で使用する設定
@@ -128,44 +126,40 @@ exit 1
 fi
 echo ""
 
+info "Check install HomeBrew"
+if [ "$(uname)" == "Darwin" ]; then
+    if ! has "brew"; then
+        info "Install HomeBrew"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        info "Already install HomeBrew"
+    fi
+brew update
+fi
+
 info "Start install the dotfiles"
 # "dotfiles/.git" がなければ "git clone" かダウンロード
-if [ ! -d ${DOT_DIRECTORY}"/.git" ]; then
-    if [ -d ${DOT_DIRECTORY} ]; then
-        rm -r ${DOT_DIRECTORY}
+if [ ! -d ${DOT_DIR}"/.git" ]; then
+    if [ -d ${DOT_DIR} ]; then
+        rm -r ${DOT_DIR}
     fi
 
-    info "Downloading dotfiles..."
-    if [ "$(uname)" == "Darwin" ]; then
-        if has "brew"; then
-            brew update
-            brew install git
-        else
-            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            sudo chown -R $(whoami) \
-                /usr/local \
-                /usr/local/var/homebrew \
-                /usr/local/var/homebrew/locks \
-                /usr/local/lib/pkgonfig \
-                /usr/local/lib/share/local \
-                /usr/local/lib/share/man
-            brew update
-            brew install git
-        fi
-    fi
-
+    info "Setting dotfiles..."
     if has "git"; then
-    # if type "git" > /dev/null 2>&1; then
         warn "Already install git"
 
     else
         warn "Install Git first"
-        if has "apt"; then
+        if has "brew"; then
+            brew install git
+        elif has "apt"; then
         # if type "apt" > /dev/null 2>&1; then
             sudo apt -y install git
         elif has "yum"; then
         # elif type "yum" > /dev/null 2>&1; then
             sudo yum -y install git
+        else
+            echo error "Can not find package control system"
         fi
     fi
 
@@ -183,34 +177,25 @@ echo ""
 case ${OSTYPE} in
 darwin*)
     # "OS X" 用設定
-    info "Check install Bash4.x or not"
-    if [ ! -e /usr/local/bin/bash ]; then
-        info "Brew install Bash4.x"
-
-        if ! has "brew"; then
-            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            # sudo chown -R $(whoami) \
-            #     /usr/local \
-            #     /usr/local/var/homebrew \
-            #     /usr/local/var/homebrew/locks \
-            #     /usr/local/lib/pkgonfig \
-            #     /usr/local/lib/share/local \
-            #     /usr/local/lib/share/man
-        fi
-        brew update
-        brew install bash > /dev/null 2>&1
-        # brew upgrade bash > /dev/null 2>&1
-        # sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
-        # chsh -s /usr/local/bin/bash
-        sudo bash -c "echo /usr/local/Celler/bash >> /etc/shells"
-        chsh -s /usr/local/Celler/bash
-
-        # ↓端末再起動要否は未確認
-        # exec $SHELL -l
-    else
-        info "Installed Bash4.x"
-    fi
-    echo ""
+    # "echo" のエラーは解消済のハズ、以下は不要
+    #    # info "Check install Bash4.x or not  " {{{
+    # if [ ! -e /usr/local/bin/bash ]; then
+    #     info "Brew install Bash4.x"
+    #
+    #     brew install bash > /dev/null 2>&1
+    #     # brew upgrade bash > /dev/null 2>&1
+    #     # sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
+    #     # chsh -s /usr/local/bin/bash
+    #     # sudo bash -c "echo /usr/local/Celler/bash >> /etc/shells"
+    #     chsh -s /usr/local/Celler/bash
+    #
+    #     # ↓端末再起動要否は未確認
+    #     # exec $SHELL -l
+    # else
+    #     info "Installed Bash4.x"
+    # fi
+    # echo ""
+    #  }}}
 
     info "Call setting OS X"
     bash ~/dotfiles/etc/test/osx/init_osx.sh
