a4b21bf172644e7f088d83406fa4cd8d55546146
wacky515 <wacky515@gmail.com>
2018-06-03 11:23:45 +0800

    Add template

--- etc/init/osx/setting_track_pad.sh
+++ etc/init/osx/setting_track_pad.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# @(#) Setting Track Pad
+# Created:     2018/06/02 00:00:00
+# Last Change: 2018/06/03 11:22:13.
+
+for f in ~/dotfiles/function/*.sh
+do
+    source ${f}
+done
+
+readonly PROCESS="setting Track Pad"
+
+gm_echo ">> ${PROCESS^}"
+# ym_echo ">> Start "
+
 defaults write -g com.apple.swipescrolldirection -bool false
+
+result_echo $? "${PROCESS}"
