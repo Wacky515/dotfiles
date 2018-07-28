a66fe84fd55e1bc54c4036efb05cbf57f2ba4c2b
wacky515 <wacky515@gmail.com>
2018-07-28 09:10:57 +0900

    Update some files

--- etc/init/osx/brew_fish.sh
+++ etc/init/osx/brew_fish.sh
@@ -13,32 +13,39 @@ done
 
 readonly PROCESS="install fish"
 
-if ! has "fish"; then
-    exit
-fi
-
 gm_echo ">> ${PROCESS^}"
 if ! has "brew"; then
     echo info "Install brew frst"
     bash ~/dotfiles/etc/init/osx/install_homebrew.sh
 fi
 
-brew install fish
-sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
-uniq /etc/shells
-chsh -s /usr/local/bin/fish
+if ! has "fish"; then
+    brew install fish
+    if [ ! grep -q "/usr/local/bin/fish" /etc/shells ]; then
+        sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
+    fi
+    # sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
+    # sort /etc/shells | uniq > /etc/shells
+    chsh -s /usr/local/bin/fish
+fi
 
-error "ここからエラー！！！"
-# curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-# sudo curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-sudo curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+# error "ここからエラー！！！"
+# FAIL: Fisherman インストール
+# Because locked ~/.config
+# if [ ! -e "~/.config/fish/functions/fisher.fish" ]; then
+fish
+if ! has "fisher"; then
+    # curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+    curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
-fisher install z
-fisher install 0rax/fish-bd
-brew install peco
-fisher oh-my-fish/plugin-peco
+    fisher install z
+    fisher install 0rax/fish-bd
+    brew install peco
+    fisher oh-my-fish/plugin-peco
 
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+    curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
-fish_update_completions
+    fish_update_completions
+fi
+
 result_echo $? "${PROCESS}"
