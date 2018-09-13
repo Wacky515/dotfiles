9a9f59c4a902e5f50c5bcc20bebbe522b94a949d
wacky515 <wacky515@gmail.com>
2018-09-14 08:54:41 +0900

    Add setting for ChgKey

---
+++ etc/init/windows/settings/chgkey/setting_chgkey.cmd
@@ -0,0 +1,73 @@
+@echo off
+rem Created:     2018/01/01 00:00:00
+rem Last Change: 2018/09/11 12:22:26.
+
+rem スクリプトがある "Dir" に "cd"
+set bat_path=%~dp0
+cd /d %bat_path%
+
+rem ---------------------------------------------------------------------------
+rem ここから追記して動作未確認
+rem ---------------------------------------------------------------------------
+
+rem 設定ファイルがある "Dir" に "cd"
+cd %OneDrive%
+
+rem ---------------------------------------------------------------------------
+rem ここまで追記して動作未確認
+rem ---------------------------------------------------------------------------
+
+echo ^>^> Setting CCleaner
+
+rem 日付取得
+set yyyy=%date:~0,4%
+set mm=%date:~5,2%
+set dd=%date:~8,2%
+
+rem 時刻取得
+set hh=%time:~0,2%
+set mi=%time:~3,2%
+set ss=%time:~6,2%
+
+set tstmp=%yyyy%-%mm%-%dd%_%hh%-%mi%-%ss%
+echo ^>^> Time stamp: %tstmp%
+
+set inidir="C:\Program Files\CCleaner\"
+set inifile="C:\Program Files\CCleaner\ccleaner.ini"
+set backup=%inidir%"\old\"%tstmp%
+
+rem "CCleaner" 停止
+echo ^>^> Kill CCleaner
+taskkill /f /im CCleaner.exe
+
+rem バックアップ 作成
+if exist %inifile% (
+    goto bkup
+) else (
+    goto mklink
+)
+
+:bkup
+echo ^>^> Backup old *.ini
+rem FIXME: Syntax error
+rem ren %inifile% ccleaner%tstmp%.ini
+move %inifile% %backup%
+
+rem シンボリックリンク 作成
+:mklink
+
+rem ---------------------------------------------------------------------------
+rem ここから追記して動作未確認
+rem ---------------------------------------------------------------------------
+
+rem echo ^>^> Copy *.ini
+rem copy "ccleaner.ini" %inifile%
+echo ^>^> Make symbolic link *.ini
+mklink %inifile% "ccleaner.ini"
+
+rem ---------------------------------------------------------------------------
+rem ここまで追記して動作未確認
+rem ---------------------------------------------------------------------------
+
+pause
+exit /b 0
