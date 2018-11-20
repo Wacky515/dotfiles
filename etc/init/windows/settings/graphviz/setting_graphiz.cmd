e746ed5eaaffb72b6fdd42dc2525bddfdf8fafc6
wacky515 <wacky515@gmail.com>
2018-11-21 08:27:32 +0900

    Update some files

--- etc/init/windows/settings/graphviz/setting_graphiz.cmd
+++ etc/init/windows/settings/graphviz/setting_graphiz.cmd
@@ -7,7 +7,7 @@ title Setting Graphiz
 
 rem スクリプトがある "Dir" に "cd"
 set bat_path=%~dp0
-pushd /d %bat_path%
+pushd %bat_path%
 
 set dir_7zip="C:\ProgramData\chocolatey\tools\7z.exe"
 
@@ -17,14 +17,15 @@ for %%i in (*.7z) do (
 
 :unzip
 if not exist C:\%~n1 (
-    echo Set %~n1 in C Drive
+    echo ^>^> Set %~n1 in C Drive
     rem %dir_7zip% e -y -oC:\%~n1 %1
     rem %dir_7zip% x -y -oC:\%~n1 %1
     %dir_7zip% x -y -oC:\ %1
     ) else (
-    echo Aleady set %~n1, skip
+    echo ^>^> Aleady set %~n1, skip
     goto eof
     )
+
 cscript setting_env_var_graphviz.vbs
 
 :eof
@@ -34,4 +35,3 @@ popd
 
 pause
 exit
-
