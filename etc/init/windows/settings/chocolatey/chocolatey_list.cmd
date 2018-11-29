e81aceef1232413df664a801495246789e2e6f36
wacky515 <wacky515@gmail.com>
2018-11-29 12:29:18 +0900

    Update some files

--- etc/init/windows/settings/chocolatey/chocolatey_list.cmd
+++ etc/init/windows/settings/chocolatey/chocolatey_list.cmd
@@ -1,7 +1,25 @@
 @echo off
+setlocal
+rem Created:     20**/**/** **:**:**
+rem Last Change: 2018/11/17 09:42:44.
 
-rem cver all -localonly
+title Output Chocolatey list
+
+rem 管理者権限で起動されたかチェック
+whoami /PRIV | find "SeLoadDriverPrivilege" > NUL
+
+rem 管理者権限ならメイン処理
+if not errorlevel 1 goto main_routine
+
+rem 管理者権限でなければ管理者権限で再起動
+@powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Start-Process %~f0 -Verb Runas"
+exit
+
+:main_routine
 clist -l
 
-pause
+endlocal
+popd
 
+rem pause
+exit /b 0
--- etc/init/windows/settings/redpen/TODO.md
+++ etc/init/windows/settings/redpen/TODO.md
@@ -1,3 +1,4 @@
 # バッチ化すること
-1. OneDriveの "redpen-1.10.1.7z" を HOMEPATH に解答
+
+1. OneDriveの "redpen-1.10.1.7z" を HOMEPATH に解凍
 2. 2 x *.vbs実行
