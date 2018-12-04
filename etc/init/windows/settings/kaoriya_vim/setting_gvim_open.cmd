c26545cea3e7ffd55a8c22228fc28488b72c616d
wacky515 <wacky515@gmail.com>
2018-12-05 07:46:59 +0900

    Update some files

--- etc/init/windows/settings/kaoriya_vim/setting_gvim_open.cmd
+++ etc/init/windows/settings/kaoriya_vim/setting_gvim_open.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 rem Created:     20**/**/** **:**:**
-rem Last Change: 2018/11/23 16:43:39.
+rem Last Change: 2018/12/04 17:47:32.
 
 title Setting GVim Open
 
@@ -15,11 +15,11 @@ rem ŠÇ—ŽÒŒ ŒÀ‚Å‚È‚¯‚ê‚ÎŠÇ—ŽÒŒ ŒÀ‚ÅÄ‹N“®
 exit
 
 :main_routine
-set homedir="C:"%HOMEPATH%
+set homedir="C:"%homepath%
 
 rem OS 64bit”Å‚Æ32bit”Å‚Å•ªŠò
-if exist ""C:"%HOMEPATH%"\vim81-kaoriya-win64\"" goto vim64
-if exist ""C:"%HOMEPATH%"\vim81-kaoriya-win32\"" (
+if exist %homedir%"\vim81-kaoriya-win64\" goto vim64
+if exist %homedir%"\vim81-kaoriya-win32\" (
     goto vim32
 ) else (
     echo ^>^> Please set GVim in home directory
@@ -28,14 +28,14 @@ if exist ""C:"%HOMEPATH%"\vim81-kaoriya-win32\"" (
 
 :vim64
 echo ^>^> Set Open by GVim 64bit
-reg add "HKEY_CLASSES_ROOT\*\shell\GVim open" /v Icon /t REG_SZ /d "\"%homedir%\vim81-kaoriya-win64\gvim.exe\"
-reg add "HKEY_CLASSES_ROOT\*\shell\GVim open\command" /t REG_SZ /d "\"%homedir%\vim81-kaoriya-win64\gvim.exe\" \"%%1\""
+reg add "HKEY_CLASSES_ROOT\*\shell\GVim open" /v "Icon" /t REG_SZ /d %homedir%"\vim81-kaoriya-win64\gvim.exe\" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\GVim open\command" /t REG_SZ /d "\"%homedir%\vim81-kaoriya-win64\gvim.exe\" \"%%1\"" /f
 goto EOF
 
 :vim32
 echo ^>^> Set Open by GVim 32bit
-reg add "HKEY_CLASSES_ROOT\*\shell\GVim open" /v Icon /t REG_SZ /d "\"%homedir%\vim81-kaoriya-win32\gvim.exe\"
-reg add "HKEY_CLASSES_ROOT\*\shell\GVim open\command" /t REG_SZ /d "\"%homedir%\vim81-kaoriya-win32\gvim.exe\" \"%%1\""
+reg add "HKEY_CLASSES_ROOT\*\shell\GVim open" /v "Icon" /t REG_SZ /d %homedir%"\vim81-kaoriya-win32\gvim.exe" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\GVim open\command" /t REG_SZ /d "\"%homedir%"\vim81-kaoriya-win32\gvim.exe" \"%%1\"" /f
 goto EOF
 
 :EOF
