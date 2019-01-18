546c1863728cb5818ceebfe8f15078ef8b7981a7
wacky515 <wacky515@gmail.com>
2019-01-18 20:34:08 +0800

    Update some files

--- etc/init/windows/settings/msys2/install_init_app.sh
+++ etc/init/windows/settings/msys2/install_init_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @(#) Install init apps in msys2
 # Created:     2018/06/07 09:13:38
-# Last Change: 2018/06/07 09:18:26.
+# Last Change: 2019/01/18 14:08:52.
 
 set -ueo pipefail
 export LC_ALL=C
@@ -11,20 +11,27 @@ do
     source ${f}
 done
 
-readonly PROCESS="install init apps in msys2"
+readonly PROCESS="install init apps in Msys2"
 
 gm_echo ">> ${PROCESS^}"
 # ym_echo ">> Start "
 
-pacman -Sy pacman
-pacman -Syu
-pacman -Su
+pacman -Syuu --noconfirm
 
-pacman -S man
-pacman -S openssh
-pacman -S tmux
-pacman -S git
-pacman -S zsh
+pacman -Sy pacman --noconfirm
+pacman -Syu --noconfirm
+pacman -Su --noconfirm
+
+pacman -Sy man --noconfirm
+pacman -Sy openssh --noconfirm
+pacman -Sy tmux --noconfirm
+pacman -Sy git --noconfirm
+pacman -Sy zsh --noconfirm
+pacman -Sy bc --noconfirm
+pacman -Sy fish --noconfirm
+
+pacman -Syu --noconfirm
+pacman -Su --noconfirm
 
 result_echo $? "${PROCESS}"
 
