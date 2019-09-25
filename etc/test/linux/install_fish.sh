3e2056ad7bf5832e9fcb28d11d311a3c93e1d2f4
脇田 純 <wacky515@gmail.com>
2019-09-25 16:36:39 +0900

    Update some files

--- etc/test/linux/install_fish.sh
+++ etc/test/linux/install_fish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # @(#) Install fish
 # Created:     2018/05/09 06:20:09
-# Last Change: 2019/09/12 12:42:24.
+# Last Change: 2019/09/25 16:36:16.
 
 source ~/dotfiles/function/result_echo.sh
 source ~/dotfiles/function/color_echo.sh
@@ -27,11 +27,12 @@ wget -L http://get.oh-my.fish | fish
 sudo apt-get install fonts-powerline
 
 # Install "peco"
-cd ~
+cd ~/Downloads
 wget https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_amd64.tar.gz
 tar zxvf peco_linux_amd64.tar.gz
 sudo cp peco_linux_amd64/peco /usr/local/bin/
 sudo chmod 777 /usr/local/bin/peco
+rm peco_linux_amd64.tar.gz*
 
 # "fish" & "peco"
 omf install peco
