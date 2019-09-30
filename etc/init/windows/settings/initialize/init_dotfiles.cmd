d6aa2b30dfc74cfc1df97197b1c3e1638f44007d
脇田 純 <wacky515@gmail.com>
2019-09-30 10:40:08 +0900

    Update some files

--- etc/init/windows/settings/initialize/init_dotfiles.cmd
+++ etc/init/windows/settings/initialize/init_dotfiles.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 rem Created:     2018/05/10 19:22:34
-rem Last Change: 2019/09/30 10:23:00.
+rem Last Change: 2019/09/30 10:39:50.
 
 set batch_title=Initialize dotfiles
 title %batch_title%
@@ -153,7 +153,12 @@ rem set rd_path=R:\E2M0\E2M-4\【秘】-E2M4-1\10.個人ファイル\Wakita\仕事\Settings
 rem robocopy %rd_path% %homepath%\OneDrive\仕事\Settings\ *.* /s /e
 rem robocopy %rd_path% C:\%homepath%\OneDrive\仕事\Settings\ /s /e
 rem robocopy R:\E2M0\E2M-4\【秘】-E2M4-1\10.個人ファイル\Wakita\仕事\Settings\ C:\Users\mm12167\OneDrive\仕事\Settings\ /s /e
-xcopy R:\E2M0\E2M-4\【秘】-E2M4-1\10.個人ファイル\Wakita\仕事\Settings C:\Users\mm12167\OneDrive\仕事\Settings /d /i
+rem xcopy R:\E2M0\E2M-4\【秘】-E2M4-1\10.個人ファイル\Wakita\仕事\Settings C:\Users\mm12167\OneDrive\仕事\Settings /d /i
+net use v: /delete
+net use v: \\M5FSV01\HONSHAB\E2M0\E2M-4\【秘】-E2M4-1\10.個人ファイル\Wakita\仕事\Settings
+robocopy v: C:\%homepath%\OneDrive\仕事\Settings\ /s /e
+net use v: /delete
+
 :inst_apps
 rem "*.config" のある "Dir" に "pushd"
 pushd %conf_path%
