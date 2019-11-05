7441b2ffe0490aeeb28fa0014446f180d75af447
脇田 純 <wacky515@gmail.com>
2019-11-05 16:34:05 +0900

    Update some files

--- etc/init/osx/settings/brew_update.sh
+++ etc/init/osx/settings/brew_update.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 # @(#) Update HomeBrew
 # Created:     2018/05/21 00:00:00
-# Last Change: 2018/05/28 15:45:52.
+# Last Change: 2019/11/05 15:42:24.
 
 for f in ~/dotfiles/function/*.sh
 do
@@ -11,19 +11,9 @@ done
 readonly PROCESS="update HomeBrew"
 gm_echo ">> ${PROCESS^}"
 
-# sudo chown -R $(whoami) \
-#     /usr/local/ \
-#     /usr/local/var/homebrew/ \
-#     /usr/local/var/homebrew/locks/ \
-#     /usr/local/lib/pkgonfig/ \
-#     /usr/local/lib/share/local/ \
-#     /usr/local/lib/share/man/
-
-# bash brew update && \
 brew update && \
 brew upgrade && \
 brew cleanup && \
 brew cask cleanup && \
 brew doctor && \
-# echo "Brew done $(hostname)" || echo "Error brew $(hostname)"
 result_echo $? "${PROCESS}"
