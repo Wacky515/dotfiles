41e9d193acf3d8a2cda0112870ad81b777ebae54
脇田 純 <wacky515@gmail.com>
2019-11-06 12:29:14 +0900

    Update some files

--- etc/init/osx/settings/brew_bash4.sh
+++ etc/init/osx/settings/brew_bash4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @(#) Install Bash 4.*
 # Created:     2019/11/05 15:38:58
-# Last Change: 2019/11/05 16:23:47.
+# Last Change: 2019/11/05 16:40:49.
 
 set -ueo pipefail
 export LC_ALL=C
@@ -15,7 +15,7 @@ readonly PROCESS="Bash 4.*"
 
 gm_echo ">> Install ${PROCESS}"
 
-if [ `bash --version | grep '2007'`]
+if [ `bash --version | grep '2007'` ] ; then
     if ! has "brew"; then
         echo info ">> Install Brew frst"
         bash ~/dotfiles/etc/init/osx/install_homebrew.sh
