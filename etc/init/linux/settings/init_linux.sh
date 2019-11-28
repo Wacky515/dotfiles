4ddf70929196a137cd6349743ff526b3999e6ce0
脇田 純 <wacky515@gmail.com>
2019-11-28 15:25:34 +0900

    Update some files

--- etc/init/linux/settings/init_linux.sh
+++ etc/init/linux/settings/init_linux.sh
@@ -1,31 +1,25 @@
 #!bin/bash
-# @(#) Initialize Linux
-# Created:     2017/12/25 **:**:**
-# Last Change: 2019/11/18 17:12:20.
+# @(#) Init Linux
+# Created:     2017/12/25 00:00:00
+# Last Change: 2019/09/25 17:10:55.
 
-set -ueo pipefail
-export LC_ALL=C
+source ~/dotfiles/function/result_echo.sh
+source ~/dotfiles/function/color_echo.sh
 
-for f in ~/dotfiles/function/*.sh
-do
-    source ${f}
-done
-
-readonly PROCESS="initialize Linux"
-
+readonly PROCESS="init Linux"
 ym_echo ">> ${PROCESS^}"
 
 # Make symbolic link
 bash ~/dotfiles/link.sh
 
 ym_echo ">> Init update"
-if has "apt"; then
-    sudo bash ./update_apt.sh
-elif has "yum"; then
-    sudo bash ./update_yum.sh
+if type "apt" > /dev/null 2>&1; then
+    sudo bash ./apt_update.sh
+elif type "yum" > /dev/null 2>&1; then
+    sudo bash ./yum_update.sh
 fi
 
-# ym_echo ">> Setting English directory name"
+ym_echo ">> Setting English directory name"
 sudo bash ./setting_eng_dir_name.sh
 
 ym_echo ">> Init install"
@@ -41,20 +35,17 @@ do
     sudo bash ./${g}
 done
 
-# ビット数、ディストリビューション 判別
-sudo chmod 777 ~/dotfiles/etc/init/linux/settings/get_distribution.sh
-declare -a info=($(~/dotfiles/etc/init/linux/settings/get_distribution.sh))
-# bits=$(uname -m)
+# ディストリビューション、ビット数 判別
+declare -a info=($(./get_distribution.sh))
 ym_echo ">> Install & setting each bit"
 if [[ ${info[1]} == "x86_64" ]]; then
-# if [[ ${bits} == "x86_64" || "amd64"]]; then
     echo ">> 64bit"
     os_bit=x64
 else
     echo ">> 32bit"
     os_bit=x32
 fi
-cd ~/dotfiles/etc/init/linux/settings/${os_bit}
+cd ./${os_bit}
 
 ym_echo ">> Install for ${os_bit}"
 for l in install_*.sh
@@ -72,59 +63,50 @@ cd -
 
 ym_echo ">> Install & setting each distribution"
 # ディストリビューション 判別
-# declare -a info=($(~/dotfiles/etc/init/linux/settings/get_distribution.sh))
+# declare -a info=($(./get_distribution.sh))
 
 case ${info[0]} in
     ubuntu)
         ym_echo ">> Distribution is Ubuntu"
-        cd ~/dotfiles/etc/init/linux/settings/ubuntu
+        cd ./ubuntu
         ;;
     debian)
         ym_echo ">> Distribution is Debian"
-        cd ~/dotfiles/etc/init/linux/settings/debian
+        cd ./debian
         ;;
     *)
         rb_echo ">> Unsupported"
-        cd ~/dotfiles/etc/init/linux/settings/unsup
         ;;
 esac
 
-ym_echo ">> Install for ${info[0]}"
+ym_echo ">> Install"
 for h in install_*.sh
 do
     sudo bash ./${h}
 done
 
-ym_echo ">> Setting for ${info[0]}"
+ym_echo ">> Setting"
 for i in setting_*.sh
 do
     sudo bash ./${i}
 done
 
 cd ./${os_bit}
-ym_echo ">> Install for ${info[0]} ${os_bit}"
+ym_echo ">> Install for ${os_bit}"
 for j in install_*.sh
 do
     sudo bash ./${j}
 done
 
-ym_echo ">> Setting for ${info[0]} ${os_bit}"
+ym_echo ">> Setting for ${os_bit}"
 for k in setting_*.sh
 do
     sudo bash ./${k}
 done
 
-cd ~/dotfiles/etc/init/linux/settings
+cd ../..
 
 ym_echo ">> Final update"
 sudo bash ./apt_update.sh
-
-ym_echo ">> Init setting"
-
-# TEST: "Dock" の "Amazon" 削除
-sudo apt remove ubuntu-web-launchers -y
-
-# TEST: "Mozc" 半角スペース 設定
-/usr/lib/mozc/mozc_tool -mode=config_dialog
 
 result_echo $? "${PROCESS}"
