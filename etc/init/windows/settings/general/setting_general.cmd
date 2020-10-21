008f2aee64ad971cb550f79492c9f7e345114415
脇田 純 <wacky515@gmail.com>
2020-10-21 16:27:03 +0900

    Update setting script Windows10

--- etc/init/windows/settings/general/setting_general.cmd
+++ etc/init/windows/settings/general/setting_general.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 rem Created:     2018/03/31 09:53:57
-rem Last Change: 2019/11/19 17:13:42.
+rem Last Change: 2020/10/21 16:12:40.
 
 set batch_title=Setting for Folder options
 title %batch_title%
@@ -23,6 +23,7 @@ pushd %bat_path%
 
 echo ^>^> %batch_title%
 
+rem "登録されている拡張子は表示しない" 無効
 echo ^>^> Show hide file extention
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
 
@@ -35,29 +36,42 @@ reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Ad
 echo ^>^> Show all folder
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "NavPaneShowAllFolders" /t REG_DWORD /d "1" /f
 
-echo ^>^> Don't make index
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Search\Preferences" /v "WholeFileSystem" /t REG_DWORD /d "1" /f
-
 echo ^>^> Show status bar
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowStatusBar" /t REG_DWORD /d "0" /f
 
 echo ^>^> Always show menus
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AlwaysShowMenus" /t REG_DWORD /d "1" /f
 
+echo ^>^> Hide task view
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d "0" /f
+
+echo ^>^> Don't make index
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Search\Preferences" /v "WholeFileSystem" /t REG_DWORD /d "1" /f
+
 echo ^>^> Don't make suffix
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v link /T REG_Binary /d 00000000 /f
 reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\NamingTemplates" /v ShortcutNameTemplate /f > nul 2>&1
 
-echo ^>^> Hide task view
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d "0" /f
-
-echo ^>^> Hide people
-reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v "HidePeopleBar" /t REG_DWORD /d "1" /f
+rem デスクトップ アイコン追加
+echo ^>^> Set desktop icons
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" /t REG_DWORD /d "0" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{645FF040-5081-101B-9F08-00AA002F954E}" /t REG_DWORD /d "0" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /t REG_DWORD /d "0" /f
 
 echo ^>^> OFF UAC
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "0" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "1" /f
+
+echo ^>^> OFF Offline files
+sc config CscService start=disabled
+
+echo ^>^> OFF Disc cache
+sc config SysMain start=disabled
+sc config UevAgentService start=demand
+
+echo ^>^> OFF Hibernate 
+powercfg.exe /hibernate off
 
 rem エクスプローラー 再起動
 taskkill /f /im explorer.exe > nul 2>&1
--- etc/init/windows/settings/general/windows10/general_setting_windows10.cmd
+++ etc/init/windows/settings/general/windows10/general_setting_windows10.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 rem Created:     2018/10/05 09:54:50
-rem Last Change: 2020/04/16 16:11:21.
+rem Last Change: 2020/10/21 16:25:51.
 
 set batch_title=General setting Windows10
 
@@ -26,17 +26,90 @@ pushd %bat_path%
 echo ^>^> %batch_title%
 rem echo ^>^> Start
 
-rem "スナップされたウィンドウのサイズを変更するときに、隣接するスナップ ウィンドウのサイズも同時に変更する" を無効にする
+echo ^>^> OFF Fast startup
+rem "高速スタートアップを有効にする" 無効
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Power" /v "HiberbootEnabled" /t REG_DWORD /d "0" /f
+
+rem "タスク バーからの People バーを削除する" 無効
+echo ^>^> OFF People
+reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v "HidePeopleBar" /t REG_DWORD /d "1" /f
+
+echo ^>^> OFF Notifications
+rem "アプリやその他の送信者からの通知を取得する" 無効
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /t REG_DWORD /d "0" /f
+rem "ロック画面に通知を表示する" 無効
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" /t REG_DWORD /d "0" /f
+rem "ロック画面にリマインダーと VoIP の着信を表示する" 無効
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK" /t REG_DWORD /d "0" /f
+rem "新機能とおすすめを確認するために、更新の後と、サインイン時にときどき、[Windows へようこそ] の情報を表示する" 無効
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-310093Enabled" /t REG_DWORD /d "0" /f
+rem "Windows を使う上でのヒントやお勧めの方法を取得する" 無効
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d "0" /f
+
+echo ^>^> Setting Strage sensor
+rem ストレージセンサ 有効
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "01" /t REG_DWORD /d "1" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "2048" /t REG_DWORD /d "7" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "4" /t REG_DWORD /d "1" /f
+rem FIXME:
+rem reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "256" /t REG_DWORD /d "1e" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v "512" /t REG_DWORD /d "0" /f
+rem 設定詳述  reg {{{
+rem ストレージセンサーの有無
+rem   0=無効
+rem   1=有効
+rem "01"=dword:00000001
+rem 実行タイミング
+rem   1e=毎月
+rem   7=毎週
+rem   1=毎日
+rem   0=ディスクの空き容量の不足時
+rem "2048"=dword:0000001e
+rem アプリで使用されていない一時ファイルを削除
+rem   0=無効
+rem   1=有効
+rem "04"=dword:00000001
+rem ごみ箱に移動してから次の期間が過ぎたファイルを削除
+rem   3c=60日
+rem   1e=30日
+rem   0e=14日
+rem   1=1日
+rem   0=許可しない
+rem "256"=dword:0000000e
+rem  ダウンロードフォルダに保存してから次の期間が過ぎたファイルを削除
+rem   3c=60日
+rem   1e=30日
+rem   0e=14日
+rem   1=1日
+rem   0=許可しない
+rem "512"=dword:0000000e}}}
+
+rem "ウィンドウをスナップしたときに横に配置できるものを表示する" 無効
+echo ^>^> OFF Snap assist
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SnapAssist" /t REG_DWORD /d "0" /f
+rem "ウィンドウをスナップするときに、利用可能な領域に合わせて自動的にサイズを変更する" 無効
+echo ^>^> OFF Snap fill
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SnapFill" /t REG_DWORD /d "0" /f
+rem "スナップされたウィンドウのサイズを変更するときに、隣接するスナップ ウィンドウのサイズも同時に変更する" 無効
+echo ^>^> OFF Joint resize
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "JointResize" /t REG_DWORD /d "0" /f
 
-rem "ウィンドウをスナップしたときに横に配置できるものを表示する" を無効にする
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SnapAssist" /t REG_DWORD /d "0" /f
+rem タイムライン 無効
+echo ^>^> OFF Time line
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d "0" /f
 
-rem "ウィンドウをスナップするときに、利用可能な領域に合わせて自動的にサイズを変更するる" を無効にする
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "SnapFill" /t REG_DWORD /d "0" /f
+rem FIXME:
+rem echo ^>^> ON Remote desktop
+rem rem リモートデスクトップ 有効
+rem reg add "HKEY_LOCAL_MACHINESYSTEMCurrentControlSetControlTerminal Server" /v "fDenyTSConnections"/t REG_DWORD /d "0" /f
+
+echo ^>^> OFF Xbox function
+sc config XblAuthManager start=disabled
+sc config XblGameSave start=disabled
+sc config XboxNetApiSvc start=disabled
 
 endlocal
 popd
 
-rem pause
+pause
 exit /b 0
