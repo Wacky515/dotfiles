16ad5e721fa162c4a622ac594f6790c7ec113693
脇田 純 <wacky515@gmail.com>
2020-10-22 10:47:02 +0900

    Update some files

--- etc/init/windows/settings/general/setting_general.cmd
+++ etc/init/windows/settings/general/setting_general.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 rem Created:     2018/03/31 09:53:57
-rem Last Change: 2020/10/21 16:12:40.
+rem Last Change: 2020/10/22 10:45:34
 
 set batch_title=Setting for Folder options
 title %batch_title%
@@ -26,29 +26,40 @@ echo ^>^> %batch_title%
 rem "登録されている拡張子は表示しない" 無効
 echo ^>^> Show hide file extention
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d "0" /f
-
-rem echo ^>^> Show hidden file, folder and drive
-rem reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d "1" /f
-
+rem "保護されたオペレーティング システム ファイルを表示しない" 無効
 echo ^>^> Show operating system
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d "1" /f
-
+rem "すべてのフォルダーを表示" 有効
 echo ^>^> Show all folder
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "NavPaneShowAllFolders" /t REG_DWORD /d "1" /f
-
+rem "ステータスバーを表示する" 有効
 echo ^>^> Show status bar
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowStatusBar" /t REG_DWORD /d "0" /f
-
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowStatusBar" /t REG_DWORD /d "1" /f
+rem "常にメニューを表示する" 有効
 echo ^>^> Always show menus
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AlwaysShowMenus" /t REG_DWORD /d "1" /f
-
+rem "タスク ビュー ボ"タンを表示 無効
 echo ^>^> Hide task view
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d "0" /f
+rem "隠しファイル、隠しフォルダー、および隠しドライブを表示しない" 有効
+echo ^>^> Hide hidden file, folder and drive
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d "2" /f
 
-echo ^>^> Don't make index
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Search\Preferences" /v "WholeFileSystem" /t REG_DWORD /d "1" /f
+rem rem "Cortana - 表示しない"
+rem echo ^>^> Hide Cortana
+rem reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "0" /f
+rem "検索ボックスを表示"
+echo ^>^> Show search box
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "2" /f
 
-echo ^>^> Don't make suffix
+rem "ファイル システムのファイル フォルダーの検索時にインデックスを使用しない" 有効
+echo ^>^> Don't use index
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Search\Preferences" /v "WholeFileSystem" /t REG_DWORD /d "0" /f
+rem "Windows search" 無効
+sc config WSearch start= disabled
+
+rem “* - ショートカット" を追加しない
+echo ^>^> Don't make shortcut name's suffix
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v link /T REG_Binary /d 00000000 /f
 reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\NamingTemplates" /v ShortcutNameTemplate /f > nul 2>&1
 
