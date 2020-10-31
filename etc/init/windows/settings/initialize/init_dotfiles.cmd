4d6882d53e925a0f43446b5e9782f184e8546134
脇田 純 <wacky515@gmail.com>
2020-11-01 02:18:27 +0900

    Update some files

--- etc/init/windows/settings/initialize/init_dotfiles.cmd
+++ etc/init/windows/settings/initialize/init_dotfiles.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 rem Created:     2018/05/10 19:22:34
-rem Last Change: 2020/11/01 01:34:32.
+rem Last Change: 2020/11/01 02:18:06.
 
 set batch_title=Initialize dotfiles
 title %batch_title%
@@ -238,9 +238,9 @@ goto install_apps
 :cp_nas
 echo ^>^> In home network, connect NAS
 set nas_settings=\\SaladStationII\share\仕事\Settings
-set nas_initapps=\\SaladStationII\shara\仕事\InitApps
+set nas_initapps=\\SaladStationII\share\仕事\InitApps
 rem set nas_settings=\\10.0.1.55\share\仕事\Settings
-rem set nas_initapps=\\10.0.1.55\shara\仕事\InitApps
+rem set nas_initapps=\\10.0.1.55\share\仕事\InitApps
 set result_nas_copy=0
 
 echo ^>^> Copy "Settings" from NAS
