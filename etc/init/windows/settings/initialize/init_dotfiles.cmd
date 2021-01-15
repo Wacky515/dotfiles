af57fb8f15d2ad3ac896ff4ece18b6fc2a1309c4
脇田 純 <wacky515@gmail.com>
2021-01-15 15:42:05 +0900

    Update some files

--- etc/init/windows/settings/initialize/init_dotfiles.cmd
+++ etc/init/windows/settings/initialize/init_dotfiles.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 rem Created:     2018/05/10 19:22:34
-rem Last Change: 2021/01/15 14:48:42.
+rem Last Change: 2021/01/15 15:41:39.
 
 set batch_title=Initialize dotfiles
 title %batch_title%
@@ -151,9 +151,9 @@ if %errorlevel% equ 0 (
 
 :chk_inst_git
 echo ^>^> Check first init or not
-echo %~n0
 if "%~n0" == "init_dotfiles_crlf" (
-    if exist C:\Program Files\Git\bin\git.exe goto git_clone
+    if exist "C:\Program Files\Git\bin\git.exe" goto git_clone
+    echo ^>^> Not install Git first init
 ) else (
     echo ^>^> Not first init
 )
