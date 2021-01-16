724c5682ff82a262ecdc55d0a42c6cf53b746a66
脇田 純 <wacky515@gmail.com>
2021-01-16 22:21:23 +0900

    Update some files

--- MacMini_update.sh
+++
@@ -1,2 +0,0 @@
-brew update && brew upgrade && brew cleanup && brew doctor && echo Brew done Mac mini || echo Error
-brew update && brew upgrade && brew cleanup && brew doctor && echo Brew done Mac mini || echo Error
